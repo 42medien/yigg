dcf804bf29a534836e80687704cf371850f710fd
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-06 11:22:23 +0000

    bug #29302

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,5 @@
 <?php
-
+echo "frontend";
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
