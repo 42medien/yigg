fe203e3a2e121d4bf58d53021e5365925d39619a
ntonu <ntonu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-09 07:58:16 +0000

    bug#30037

--- apps/frontend/modules/system/templates/_faqLinks.php
+++ apps/frontend/modules/system/templates/_faqLinks.php
@@ -4,7 +4,7 @@
   <li><?php echo link_to('Impressum', '@about_pages?template=impressum', array('title' => 'Impressum')); ?></li>
   <li><?php echo link_to("YiGG-Blog","http://blog.yigg.de",array("title" => "Neuigkeiten über YiGG")); ?></li>
   <li><?php echo link_to('YiGG Hilfecenter', 'http://help.yigg.de', array('rel' => 'help', 'title' => 'Oft gestellte Fragen')); ?></li>
-  <li><?php echo link_to('YiGGSponsorings-FAQ', '@about_pages?template=yigg-sponsoring', array('title' => 'YiggSponsorings')) ?></li>
+  <!--<li><?php echo link_to('YiGGSponsorings-FAQ', '@about_pages?template=yigg-sponsoring', array('title' => 'YiggSponsorings')) ?></li>-->
   <li><?php echo link_to('Verhaltensrichtlinien', '@legal_pages?template=verhaltensrichtlinien', array('title' => 'Verhaltensrichtlinien')); ?></li>
   <li><?php echo link_to('Nutzungsbedingungen' ,'@legal_pages?template=nutzungsbedingungen', array('title' => 'Nutzungsbedingungen')); ?></li>
   <li><?php echo link_to('Datenschutzrichtlinien' ,'@legal_pages?template=datenschutzrichtlinien', array('title' => 'Datenschutzrichtlinien')); ?></li>
