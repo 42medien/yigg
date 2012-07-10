c91b27c9d309e07b10263f436ad60837091149fa
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-10 14:02:11 +0000

    bug #30176

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -77,5 +77,14 @@
             })();
 
         </script>
+
+        <script type="text/javascript">
+            var uvOptions = {};
+            (function() {
+                var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+                uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://' ) + 'widget.uservoice.com/ivhHCap8jZkAWPJveHWCaw.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+            })();
+        </script>
   </body>
 </html>
