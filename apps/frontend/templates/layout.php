f6cc79d1a53f55d777be22afa7a7c2b234fd1895
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-10 08:50:12 +0000

    bug #30086

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -44,6 +44,9 @@
         <div class="twoThree-right">
           <?php include_partial("user/userinfo"); ?>
           <?php include_component("story", "bestVideos", array( "height"=> 285, "width" => 370)); ?>
+          <?php if(true === has_slot("sidebar_sponsoring")): ?>
+            <?php include_slot("sidebar_sponsoring"); ?>
+          <?php endif; ?>  
           <?php if(true === has_slot("sidebar")): ?>
             <?php include_slot("sidebar"); ?>
           <?php endif; ?>
