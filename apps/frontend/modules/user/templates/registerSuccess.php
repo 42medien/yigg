687d847a2d762466acda6247ffb81b38b9cdc616
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-11 12:32:31 +0000

    bug #29302

--- apps/frontend/modules/user/templates/registerSuccess.php
+++ apps/frontend/modules/user/templates/registerSuccess.php
@@ -6,48 +6,49 @@
   <p class="error">Wir konnten Dir keinen Bestätigungslink schicken. Bitte überprüfe und korrigiere Deine email-Adresse <?php echo $user->email; ?>
    Dein Konto kann nur nach Anmeldung mit einer bestätigten email-Adresse freigeschaltet werden</p>
 <?php else: ?>
+<!-- START FACEBOOK JAVASCRIPT SDK -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId        : <?php echo sfConfig::get('app_facebook_app_id') ?>, // stored in app.yml for convenience
+            status       : false, // check login status (we don't make use of this)
+            cookie       : true, // enable cookies to allow the server to access the session
+            xfbml        : true,  // parse XFBML
+            oauth        : true
+        });
+    };
+
+    // Load the SDK Asynchronously
+    (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
+
+    function onClickloginfb() {
+        //Use the FB object's login method from the Facebook Javascript SDK to authenticate the user
+        //If the user has already approved your app, she is simply logged in
+        //If not, the app authentication dialog box is shown
+        FB.login(function(response) {
+
+            //If the user is succesfully authenticated, we execute some code to handle the freshly
+            //logged in user, if not, we do nothing
+            if (response.authResponse) {
+                window.location = "/fb_login"
+            }
+        }, {scope:'email'});  //we just ask for the email permission
+    }
+</script>
+<!-- END FACEBOOK JAVASCRIPT SDK -->
+
+<button onclick="onClickloginfb();">Login</button>
   <form id="RegisterForm" class="ninjaForm" action="<?php echo url_for("@user_register"); ?>" method="post">
     <fieldset>
 
-        <!-- START FACEBOOK JAVASCRIPT SDK -->
-        <div id="fb-root"></div>
-        <script>
-            window.fbAsyncInit = function() {
-                FB.init({
-                    appId        : <?php echo sfConfig::get('app_facebook_app_id') ?>, // stored in app.yml for convenience
-                    status       : false, // check login status (we don't make use of this)
-                    cookie       : true, // enable cookies to allow the server to access the session
-                    xfbml        : true,  // parse XFBML
-                    oauth        : true
-                });
-            };
 
-            // Load the SDK Asynchronously
-            (function(d){
-                var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-                if (d.getElementById(id)) {return;}
-                js = d.createElement('script'); js.id = id; js.async = true;
-                js.src = "//connect.facebook.net/en_US/all.js";
-                ref.parentNode.insertBefore(js, ref);
-            }(document));
-
-            function onClickloginfb() {
-                //Use the FB object's login method from the Facebook Javascript SDK to authenticate the user
-                //If the user has already approved your app, she is simply logged in
-                //If not, the app authentication dialog box is shown
-                FB.login(function(response) {
-
-                    //If the user is succesfully authenticated, we execute some code to handle the freshly
-                    //logged in user, if not, we do nothing
-                    if (response.authResponse) {
-                        window.location = "/fb_login"
-                    }
-                }, {scope:'email'});  //we just ask for the email permission
-            }
-        </script>
-        <!-- END FACEBOOK JAVASCRIPT SDK -->
-
-        <button onclick="onClickloginfb()">Login</button>
 
       <?php echo $form->render();?>
       <h3 style="margin-left:150px;">Nutzungsbedingungen</h3>
