f3b1524a80065838e969029807d367c2f0b01fda
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-11 13:38:43 +0000

    bug #30284

--- apps/frontend/modules/story/templates/_bestVideos.php
+++ apps/frontend/modules/story/templates/_bestVideos.php
@@ -2,16 +2,7 @@
   <?php echo $sf_data->get('video', ESC_RAW)->getSizedCode($width,$height); ?>
 <?php else:?>
   <div class="sidead">
-<!--<script type="text/javascript"><!--
-google_ad_client = "pub-1406192967534280";
-/* 336x280, Erstellt 24.05.11 */
-google_ad_slot = "3232167360";
-google_ad_width = 336;
-google_ad_height = 280;
-//-->
-<!--</script>-->
-<!-- <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"> 
-</script>-->    
+    
   <script type='text/javascript'>
     var googletag = googletag || {};
     googletag.cmd = googletag.cmd || [];
