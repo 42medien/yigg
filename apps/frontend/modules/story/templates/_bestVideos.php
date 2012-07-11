9490a9a2d1e6f6749146cc76b739c88aa1bdc755
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-11 15:28:06 +0000

    bug #30284

--- apps/frontend/modules/story/templates/_bestVideos.php
+++ apps/frontend/modules/story/templates/_bestVideos.php
@@ -3,20 +3,74 @@
 <?php else:?>
   <div class="sidead">      
     <!-- Yigg-Button-1L -->
-    <div id='div-gpt-ad-1342004948184-0' style='width:150px; height:125px;'>
+    <div id='div-gpt-ad-1342004948184-0' style='width:170px; height:125px;'>
     <script type='text/javascript'>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-0'); });
     </script>
     </div>
 
     <!-- Yigg-Button-1R -->
-    <div id='div-gpt-ad-1342004948184-1' style='width:150px; height:125px;'>
+    <div id='div-gpt-ad-1342004948184-1' style='width:170px; height:125px;'>
     <script type='text/javascript'>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-1'); });
     </script>
     </div>
 
-    
+    <!-- Yigg-Button-2L -->
+    <div id='div-gpt-ad-1342004948184-2' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-2'); });
+    </script>
+    </div>
+
+    <!-- Yigg-Button-2R -->
+    <div id='div-gpt-ad-1342004948184-3' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-3'); });
+    </script>
+    </div>
+
+    <!-- Yigg-Button-3L -->
+    <div id='div-gpt-ad-1342004948184-4' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-4'); });
+    </script>
+    </div>
+
+    <!-- Yigg-Button-3R -->
+    <div id='div-gpt-ad-1342004948184-5' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-5'); });
+    </script>
+    </div>
+
+    <!-- Yigg-Button-4L -->
+    <div id='div-gpt-ad-1342004948184-6' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-6'); });
+    </script>
+    </div>
+
+    <!-- Yigg-Button-4R -->
+    <div id='div-gpt-ad-1342004948184-7' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-7'); });
+    </script>
+    </div>
+
+    <!-- Yigg-Button-5L -->
+    <div id='div-gpt-ad-1342004948184-8' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-8'); });
+    </script>
+    </div>
+
+    <!-- Yigg-Button-5R -->
+    <div id='div-gpt-ad-1342004948184-9' style='width:170px; height:125px;'>
+    <script type='text/javascript'>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-9'); });
+    </script>
+    </div>
 
 </div>
 <?php endif;?>
