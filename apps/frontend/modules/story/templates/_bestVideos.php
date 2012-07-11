99c40fec60e62145711c348e4b4d89d026457f83
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-11 15:30:06 +0000

    bug #30284

--- apps/frontend/modules/story/templates/_bestVideos.php
+++ apps/frontend/modules/story/templates/_bestVideos.php
@@ -3,7 +3,7 @@
 <?php else:?>
   <div class="sidead">      
     <!-- Yigg-Button-1L -->
-    <div id='div-gpt-ad-1342004948184-0' style='width:170px; height:125px;'>
+    <div id='div-gpt-ad-1342004948184-0' style='width:170px; height:125px; float:left;'>
     <script type='text/javascript'>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1342004948184-0'); });
     </script>
