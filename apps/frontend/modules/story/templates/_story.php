5ff78cce6db7f57b0c2f60874eaa2b0be5c310ca
ntonu <ntonu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-11 14:47:33 +0000

    bug#30314

--- apps/frontend/modules/story/templates/_story.php
+++ apps/frontend/modules/story/templates/_story.php
@@ -5,7 +5,7 @@
 
   <?php if($story->type == Story::TYPE_NORMAL):?>
     <div class="screenshot">
-      <?php echo link_to(img_tag("http://stromboli.yigg.de/?url=" . $story->external_url, array("width" =>  120, "height" => 88)), $story->external_url, array("title" => $story->title, "rel" => "external"));?>
+      <?php echo img_tag("http://stromboli.yigg.de/?url=" . $story->external_url, array("width" =>  120, "height" => 88));?>
     </div>
   <?php endif; ?>
    <h3 class="entry-title">
