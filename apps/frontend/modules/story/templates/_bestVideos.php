9dfd9866925a4807af105cbda2ec7c7ada0664f5
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-11 13:42:38 +0000

    bug #30284

--- apps/frontend/modules/story/templates/_bestVideos.php
+++ apps/frontend/modules/story/templates/_bestVideos.php
@@ -1,41 +1,7 @@
 <?php if(false !== $video):?>
   <?php echo $sf_data->get('video', ESC_RAW)->getSizedCode($width,$height); ?>
 <?php else:?>
-  <div class="sidead">
-    
-  <script type='text/javascript'>
-    var googletag = googletag || {};
-    googletag.cmd = googletag.cmd || [];
-    (function() {
-    var gads = document.createElement('script');
-    gads.async = true;
-    gads.type = 'text/javascript';
-    var useSSL = 'https:' == document.location.protocol;
-    gads.src = (useSSL ? 'https:' : 'http:') + 
-    '//www.googletagservices.com/tag/js/gpt.js';
-    var node = document.getElementsByTagName('script')[0];
-    node.parentNode.insertBefore(gads, node);
-    })();
-    </script>
-
-    <script type='text/javascript'>
-    googletag.cmd.push(function() {
-    googletag.defineSlot('/1043423/Yigg-Button-1L', [170, 125], 'div-gpt-ad-1342004948184-0').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-1R', [170, 125], 'div-gpt-ad-1342004948184-1').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-2L', [170, 125], 'div-gpt-ad-1342004948184-2').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-2R', [170, 125], 'div-gpt-ad-1342004948184-3').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-3L', [170, 125], 'div-gpt-ad-1342004948184-4').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-3R', [170, 125], 'div-gpt-ad-1342004948184-5').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-4L', [170, 125], 'div-gpt-ad-1342004948184-6').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-4R', [170, 125], 'div-gpt-ad-1342004948184-7').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-5L', [170, 125], 'div-gpt-ad-1342004948184-8').addService(googletag.pubads());
-    googletag.defineSlot('/1043423/Yigg-Button-5R', [170, 125], 'div-gpt-ad-1342004948184-9').addService(googletag.pubads());
-    googletag.pubads().enableSingleRequest();
-    googletag.enableServices();
-    });
-    </script>
-
-
+  <div class="sidead">      
     <!-- Yigg-Button-1L -->
     <div id='div-gpt-ad-1342004948184-0' style='width:170px; height:125px;'>
     <script type='text/javascript'>
