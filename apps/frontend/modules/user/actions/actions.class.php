17b668b4faabf286b3bd2740e6d365afa7858a58
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-13 08:20:32 +0000

    bug #29302

--- apps/frontend/modules/user/actions/actions.class.php
+++ apps/frontend/modules/user/actions/actions.class.php
@@ -373,7 +373,7 @@ class userActions extends yiggActions
                             $user->setFacebookId($facebook_user['id']);
                             $user->save();
                         }
-                        print_r($facebook_user['id']);
+                        print_r($facebook_user);
                         print_r($user->getFacebookId());
                     }
                     die;
