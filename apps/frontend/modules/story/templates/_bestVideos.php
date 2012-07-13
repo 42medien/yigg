727ab4ed2f1185580bf7653e8ac0e2d059e9a4c7
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-13 09:18:12 +0000

    bug #30457

--- apps/frontend/modules/story/templates/_bestVideos.php
+++ apps/frontend/modules/story/templates/_bestVideos.php
@@ -1,14 +1,7 @@
 <?php if(false !== $video):?>
   <?php echo $sf_data->get('video', ESC_RAW)->getSizedCode($width,$height); ?>
 <?php else:?>
-<script type='text/javascript'>
-    
-        var html_body = $('#div-gpt-ad-1342004948184-0').html();
-        alert(html_body);
-    
-</script>
-
-  <div class="sidead" style="width: auto !important; padding-left: 10px; ">      
+  <div class="sidead" style="width: auto !important; padding-left: 10px;">
     <!-- Yigg-Button-1L -->
     <div id='div-gpt-ad-1342004948184-0' style='width:170px; height:125px; float:left;'>
     <script type='text/javascript'>
