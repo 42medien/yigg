85435b422192749124c2179a6d3c6273f0c3fcae
ntonu <ntonu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-25 09:32:52 +0000

    bug#31160

--- apps/frontend/modules/story/templates/storyBarSuccess.php
+++ apps/frontend/modules/story/templates/storyBarSuccess.php
@@ -2,7 +2,7 @@
     <div id="bar_content">
         <div id="bar_logo">
             <?php
-            echo link_to(img_tag('YiGG_Logo.png', array(
+            echo link_to(img_tag('YiGG-Logo.png', array(
                     'alt' => 'YiGG Nachrichten zum Mitmachen: Lesen - Bewerten - Schreiben',
                     'width' => 191,
                     'height' => 54
