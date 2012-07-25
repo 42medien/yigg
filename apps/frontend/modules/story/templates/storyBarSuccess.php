29fa5e03ccbf2ad30b04a1cef96e3fe17ecd6c4e
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-25 09:46:18 +0000

    bug #29099

--- apps/frontend/modules/story/templates/storyBarSuccess.php
+++ apps/frontend/modules/story/templates/storyBarSuccess.php
@@ -24,6 +24,15 @@
                 <?php include_component("comment", "commentList", array("obj" => $story, "inlist" => isset($inlist)  ? $inlist : false)); ?>
             </div>
         </div>
+        <div class="spreadly-button">
+            <iframe src="http://button.spread.ly/?url=<?php echo $story->external_url;?>&social=1&title=<?php echo $story->title;?>"
+                    style="overflow:hidden; width: 420px; height: 30px; padding: 0px 0;"
+                    frameborder="0"
+                    scrolling="no"
+                    marginheight="0"
+                    allowTransparency="true">
+            </iframe>
+        </div>
         <div class="close" onclick="redirect()">Close</div>
         <script type="text/javascript">
             <!--
