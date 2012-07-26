a259abed837eebdef505f1f5973a42fcabaad823
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-26 15:45:15 +0000

    bug #29099

--- apps/frontend/modules/story/templates/_story.php
+++ apps/frontend/modules/story/templates/_story.php
@@ -21,8 +21,7 @@
 
    <h3 class="entry-title">
      <?php if($sf_request->getModuleAction() === "story/show")://$story["type"] == Story::TYPE_NORMAL?>
-       <?php echo link_to($story->title, $story["external_url"], array("title" => $story->title, "rel" => "external"));?>
-       <?php //echo link_to($story->title, url_for_story($story, "bar"), array("title" => $story->title, 'target' => '_blank'));?>
+       <?php echo link_to($story->title, url_for_story($story, "bar"), array("title" => $story->title, 'target' => '_blank'));?>
      <?php else:?>
        <?php echo link_to_story($story->title, $story, array("title" => $story->title));?>
      <?php endif; ?>
@@ -63,8 +62,8 @@
 
          $external_url_title = parse_url(str_replace('www.','',$story["external_url"]))
          ?>
-         <?php echo link_to('mehr bei '.$external_url_title['host'], $story["external_url"], array("title" => $story->title, "rel" => "external"));?>
-         <?php //echo link_to('mehr bei '.$external_url_title['host'], url_for_story($story, "bar"), array("title" => $story->title, "rel" => "nofollow", 'target' => '_blank'));?>
+         <?php //echo link_to($story->title, $story["external_url"], array("title" => $story->title, "rel" => "external"));?>
+         <?php echo link_to('mehr bei '.$external_url_title['host'], url_for_story($story, "bar"), array("title" => $story->title, "rel" => "nofollow", 'target' => '_blank'));?>
      </p>
 
         <?php if(isset($total) && $total > 9 &&
--- apps/frontend/modules/story/templates/storyBarSuccess.php
+++ apps/frontend/modules/story/templates/storyBarSuccess.php
@@ -43,7 +43,7 @@
         <div class="bar_plholder"></div>
         <div class="v_separator"></div>
         <div id="bar_related_stories">
-            <?php include_partial('relatedStories',  array('stories' => $relatedStories)); ?>
+            <?php //include_partial('relatedStories',  array('stories' => $relatedStories)); ?>
         </div>
         <div class="v_separator"></div>
         <div class="close" onclick="redirect()"></div>
