e7582216756e5ac0fc17d56cfaa4621f96e8f167
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-26 10:56:18 +0000

    bug #29099

--- apps/frontend/modules/story/templates/_relatedStories.php
+++ apps/frontend/modules/story/templates/_relatedStories.php
@@ -6,6 +6,7 @@
         "@user_public_profile?username={$story->Author->username}");
     echo link_to($story['title'], url_for_story($story, "bar"), array("title" => $story->title, "rel" => "nofollow"));
     ?>
+    <div id="show_stories">Toogle</div>
 </div>
 <div id="bar_related_stories_content">
     <ol class="avatar-list-style">
@@ -25,7 +26,7 @@
     $(function() {
         $("#bar_related_stories_content").hide();
 
-        $("#bar_related_stories_label").click(function() {
+        $("#show_stories").click(function() {
             $("#bar_related_stories_content").toggle();
         });
 
