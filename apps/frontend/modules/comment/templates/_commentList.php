b885d265d708af56155cef20322f9f39e8ea8c7f
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-27 11:50:22 +0000

    bug #31342

--- apps/frontend/modules/comment/templates/_commentList.php
+++ apps/frontend/modules/comment/templates/_commentList.php
@@ -1,8 +1,6 @@
 <?php
     use_helper("Date");
-echo "at1:". $at_beginning;
     $at_beginning = isset($at_beginning)?$at_beginning:false;
-echo "at:". $at_beginning;
 ?>
 <?php if(false === $sf_request->isAjaxRequest() || "show" === $sf_request->getAction()):?>
   <div id="<?php echo strtolower(get_class($sf_data->getRaw("obj"))); ?>-comments-<?php echo $obj['id']; ?>" class="clr">
