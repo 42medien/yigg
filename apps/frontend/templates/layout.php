4e08dff62dcf168299d672d634221f7f95bedb06
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-31 09:00:05 +0000

    bug #30457

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -31,7 +31,7 @@
         googletag.defineSlot('/1043423/Yigg-Button-5L', [170, 125], 'div-gpt-ad-1342004948184-8').addService(googletag.pubads());
         googletag.defineSlot('/1043423/Yigg-Button-5R', [170, 125], 'div-gpt-ad-1342004948184-9').addService(googletag.pubads());
         googletag.pubads().enableSingleRequest();
-        googletag.pubads().collapseEmptyDivs();
+        //googletag.pubads().collapseEmptyDivs();
         googletag.enableServices();
         });
     </script>
