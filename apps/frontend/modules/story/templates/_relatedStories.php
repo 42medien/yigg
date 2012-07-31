01b89473bd798b8064cb2ed7996f7df6ad557100
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-07-31 11:18:06 +0000

    bug #31520

--- apps/frontend/modules/story/templates/_relatedStories.php
+++ apps/frontend/modules/story/templates/_relatedStories.php
@@ -1,34 +1,25 @@
 <div id="bar_related_stories_label">
-    <?php
-    $story = $stories->get(0);
-    if($bar){
+    <div class="related_stories">
+        <?php
+        $story = $stories->get(0);
         echo link_to(
             avatar_tag($story->Author->Avatar, "icon.gif", 35,35),
             "@user_public_profile?username={$story->Author->username}");
         echo "Read Related Stories:";
         echo link_to($story['title'], url_for_story($story, "bar"), array("title" => $story->title, "rel" => "nofollow"));
         ?>
-        <div id="show_stories">Toogle</div>
-    <?php }else{
-        ?>
-        <div style="font-size: 108% !important; font-weight: bold; margin-top: 10px; margin-bottom:3px;">Das könnte Sie auch interessieren:</div>
-    <?php } ?>
+    </div>
+    <div id="show_stories">Toogle</div>
+    <div class="clear"></div>
 </div>
 <div id="bar_related_stories_content">
     <ol class="avatar-list-style">
-        <?php foreach ( $stories as $story): ?>
-        <li>
-            <?php
-            if($bar){
-                echo link_to(
-                    avatar_tag($story->Author->Avatar, "icon.gif", 14,14),
-                    "@user_public_profile?username={$story->Author->username}");
-                echo link_to($story['title'], url_for_story($story, "bar"), array("title" => $story->title, "rel" => "nofollow"));
-            }else{
-                echo link_to($story['title'], url_for_story($story), array("title" => $story->title, "rel" => "nofollow"));
-            }
-        ?>
-        </li>
+        <?php foreach ( $stories as $story): ?><li><?php
+        echo link_to(
+            avatar_tag($story->Author->Avatar, "icon.gif", 14,14),
+            "@user_public_profile?username={$story->Author->username}");
+        echo link_to($story['title'], url_for_story($story, "bar"), array("title" => $story->title, "rel" => "nofollow"));
+        ?></li>
         <?php endforeach; ?>
     </ol>
 </div>
@@ -36,13 +27,13 @@
 <script type="text/javascript">
     <!--
     $(function() {
-        if($("#bar_related_stories_content")){
-            $("#bar_related_stories_content").hide();
+        $("#bar_related_stories_content").hide();
 
-            $("#show_stories").click(function() {
-                $("#bar_related_stories_content").toggle();
-            });
-        }
+        $("#show_stories").click(function() {
+            $("#bar_related_stories_content").toggle();
+        });
+
+
     });
     //-->
 </script>
