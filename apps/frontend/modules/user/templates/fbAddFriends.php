787a8085cb576d7445d701ff3d42b63ec4527e55
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-08-07 08:46:09 +0000

    bug #29302

--- apps/frontend/modules/user/templates/fbAddFriends.php
+++ apps/frontend/modules/user/templates/fbAddFriends.php
@@ -3,12 +3,24 @@
 <script src="http://connect.facebook.net/en_US/all.js">
 </script>
 <script>
-    FB.init({
-        appId:<?php echo sfConfig::get('app_facebook_app_id') ?>,
-        cookie:true,
-        status:true,
-        xfbml:true
-    });
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId        : <?php echo sfConfig::get('app_facebook_app_id') ?>,
+            status       : false,
+            cookie       : true,
+            xfbml        : true,
+            oauth        : true
+        });
+    };
+
+    // Load the SDK Asynchronously
+    (function(d){
+        var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement('script'); js.id = id; js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
 
     FB.ui({ method: 'apprequests',
         message: 'Here is a new Requests dialog...'});
