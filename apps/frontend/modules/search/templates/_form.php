51fee7d0c4c49b35cef7815a36091004a852ee8b
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-08-08 09:41:54 +0000

    bug #30905

--- apps/frontend/modules/search/templates/_form.php
+++ apps/frontend/modules/search/templates/_form.php
@@ -1,6 +1,4 @@
 <form action="<?php echo url_for("@search");?>" method="get" id="searchForm">
-  <fieldset>
     <?php echo $form;?>
     <input type="submit" value="Suchen" class="button"/>
-  </fieldset>
 </form>
