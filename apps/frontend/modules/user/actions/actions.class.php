2ef6a08b4732d3aebd7096ad43f24038116be7a5
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-08-10 08:31:16 +0000

    bug #29302

--- apps/frontend/modules/user/actions/actions.class.php
+++ apps/frontend/modules/user/actions/actions.class.php
@@ -479,13 +479,12 @@ class userActions extends yiggActions
 
         $facebook_user = $facebook->getUser();
 
-        print_r($facebook_user); die;
         if ($facebook_user)
         {
             try
             {
                 $facebook_user_profile = $facebook->api('/me');
-
+                print_r($facebook_user_profile); die;
                 if(!is_null($facebook_user_profile['email']))
                 {
 
