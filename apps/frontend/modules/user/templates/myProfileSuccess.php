8e10bd2594e65f9a43b28d9ee799a0672bf9c5b1
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-08-18 07:53:19 +0000

    bug #27137

--- apps/frontend/modules/user/templates/myProfileSuccess.php
+++ apps/frontend/modules/user/templates/myProfileSuccess.php
@@ -2,7 +2,7 @@
 <?php include_partial("system/systemMessages")?>
 <form enctype="multipart/form-data" method="post" action="<?php echo url_for("@user_my_profile?view=profil"); ?>"  id="new_form" class="profileForm">
   <fieldset>
-    <?php echo $profile_form->render() ?>
+    <?php //echo $profile_form->render() ?>
   </fieldset>
   <div class="actions">
     <input type="submit" value="Profil bearbeiten" class="button" id="save" />
