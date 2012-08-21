dfab15d938e1cfd6d8cc531b0024cf3ab681e2f3
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-08-21 09:09:58 +0000

    bug #33030

--- apps/frontend/modules/story/templates/_rateStory.php
+++ apps/frontend/modules/story/templates/_rateStory.php
@@ -36,7 +36,12 @@
       <!-- RatingForm -->
       <?php $hasRated = ( null === $story->hasRated() ? $sf_user->hasRated($story['id']) : $story->hasRated() );
         if (false === $hasRated ):?>
-        <form id="StoryRate<?php echo $story->id ?>" class="rating-form ninjaForm ninjaAjaxSubmit" action="<?php echo url_for_story($story, "rate"); ?>" method="post"><fieldset><h4><?php echo $story->currentRating(); ?></h4><p>YiGGs</p><input type="hidden" name="StoryRate[_csrf_token]" value="<?php echo $form[$form->getCSRFFieldName()]->getValue() ?>" id="StoryRate<?php echo $story->id; ?>__csrf_token" /><input name="StoryRate[ratingtoken]" id="StoryRate_ratingtoken<?php echo $story->id; ?>" type="hidden" value="<?php echo md5(rand(1,10000));?>" /><input type="submit" value="YiGG it!" class="button" id="Rate<?php echo $story->id?>" /></fieldset></form>
+        <form id="StoryRate<?php echo $story->id ?>" class="rating-form ninjaForm ninjaAjaxSubmit" action="<?php echo url_for_story($story, "rate"); ?>" method="post">
+            <fieldset>
+<!--                /*<p>YiGGs</p>*/-->
+                <input type="hidden" name="StoryRate[_csrf_token]" value="<?php echo $form[$form->getCSRFFieldName()]->getValue() ?>" id="StoryRate<?php echo $story->id; ?>__csrf_token" /><input name="StoryRate[ratingtoken]" id="StoryRate_ratingtoken<?php echo $story->id; ?>" type="hidden" value="<?php echo md5(rand(1,10000));?>" /><input type="submit" value="YiGG it!" class="button" id="Rate<?php echo $story->id?>" />
+                <h4><label></label><?php echo $story->currentRating(); ?></h4>
+            </fieldset></form>
       <?php else: ?>
        <div class="<?php if(false === $sf_request->isAjaxRequest() || true === isset($completeStory)):?>rating-form <?php endif;?>voted">   
 <!--      <p>YiGGs</p>-->
