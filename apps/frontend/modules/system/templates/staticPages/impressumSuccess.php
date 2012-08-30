0a8838f42a7276c37910ec498cf0b888914593c6
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-08-30 08:32:03 +0000

    bug #33734

--- apps/frontend/modules/system/templates/staticPages/impressumSuccess.php
+++ apps/frontend/modules/system/templates/staticPages/impressumSuccess.php
@@ -6,8 +6,7 @@
 <strong>Telefon:</strong> 06201 . 845 200<br />
  <strong>Fax:</strong> 06201 . 845 20 29<br />
 <strong>Geschäftsführer:</strong> Marco Ripanti<br />
-<strong>Handelsregister:</strong> beantragt<br />
-<strong>HRB:</strong> 715271<br />
+<strong>Handelsregister:</strong> 715271<br />
 <strong>USt-IdNr.:</strong> beantragt<br />
 <strong>Gerichtsstand:</strong> Mannheim<br />
 <p>
