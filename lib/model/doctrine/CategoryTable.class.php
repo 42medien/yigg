ccaa7c853f33b26e4015ea6bd17e3daec10e430e
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-04 12:38:07 +0000

    bug #33596

---
+++ lib/model/doctrine/CategoryTable.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ * @package     yigg
+ * @subpackage  story
+ */
+class CategoryTable extends Doctrine_Table
+{
+    /**
+     * return instance of current table
+     *
+     * @return Doctrine_Table
+     */
+    public static function getTable()
+    {
+        return Doctrine::getTable('Category');
+    }
+
+    public function getCategories()
+    {
+        $q = Doctrine_Query::create()
+                ->from('Category')->execute();
+
+        return $q;
+    }
+}
