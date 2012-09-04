91c56fcc4479e0670465cd1210b32822e4144dec
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-04 15:14:28 +0000

    bug #33596

--- lib/yiggStoryFinder.php
+++ lib/yiggStoryFinder.php
@@ -198,8 +198,6 @@ class yiggStoryFinder
      */
     public function confineWithCategory($category_id)
     {
-        print_r("cat:".$category_id);
-
         $this->joins['category'] = 'INNER JOIN story_category on s.id = story_category.story_id';
         return $this->addWhere('story_category.category_id = ?', $category_id);
     }
