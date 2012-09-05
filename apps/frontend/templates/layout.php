499e6647c91e64ef420182e61de6b8b38e360bd9
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-05 14:35:18 +0000

    bug #33593

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -112,14 +112,14 @@
                 <div class="login_box">
                     <div class="login_box_cont">
                         <?php include_partial("system/navigation"); ?>
+                        <div class="login_fb"></div>
                         <div class="login_link">
                             <a href="#">Login</a>
                             <div class="login_box">
                                 <a class="fb_cnct" href="#" onclick="onClickloginfb(); return false;"></a>
                                 <a class="yigg_cnct" href="<?php echo url_for('@user_login');?>"></a>
                             </div>
-                        </div>
-                        <div class="login_fb"></div>
+                        </div>                        
                     </div>
                     <div class="clear"></div>
                 </div>
