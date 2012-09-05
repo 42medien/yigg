39ce6b1f829da31652ac34dbc3df8e993a6b8f5b
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-05 12:43:22 +0000

    bug #33922

--- lib/forms/yiggWidgetFormSchemaFormatter.class.php
+++ lib/forms/yiggWidgetFormSchemaFormatter.class.php
@@ -8,4 +8,17 @@ class yiggWidgetFormSchemaFormatter extends sfWidgetFormSchemaFormatter
     $namedErrorRowFormatInARow 	= "<span class=\"error_message\">%name%: %errors%</span>\n",
     $helpFormat      			= '%help%',
     $decoratorFormat 			= "%content%";
+
+    public function formatRow($label, $field, $errors = array(), $help = '', $hiddenFields = null)
+    {
+        $rowFormat = strtr($this->getRowFormat(), array(
+            '%label%'         => $label,
+            '%field%'         => $field,
+            '%error%'         => $this->formatErrorsForRow($errors),
+            '%help%'          => $this->formatHelp($help),
+            '%hidden_fields%' => null === $hiddenFields ? '%hidden_fields%' : $hiddenFields,
+        ));
+
+        return $rowFormat;
+    }
 }
