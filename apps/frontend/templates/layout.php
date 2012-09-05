70d9abadc1bcec335068dcb2f448c71a434e9281
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-05 10:08:04 +0000

    bug #33922

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -117,6 +117,7 @@
                             <div class="login_box">
                                 <a class="fb_cnct" href="#" onclick="onClickloginfb(); return false;"></a>
                                 <?php echo link_to('Login', 'user_login', array('class' => 'yigg_cnct'));?>
+                                <a class="yigg_cnct" href="<?php echo url_for('@user_login');?>"></a>
                             </div>
                         </div>
                     </div>
