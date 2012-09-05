587da345ee9768550a07ac0316f22b3107e635b7
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-05 09:47:08 +0000

    bug #33593

--- apps/frontend/modules/user/templates/resetPasswordSuccess.php
+++ apps/frontend/modules/user/templates/resetPasswordSuccess.php
@@ -1,10 +1,10 @@
-<h1 class="log_title">Passwort vergessen?</h1>
 
 <?php include_partial("system/systemMessages"); ?>
 
 <?php if(false === $form->isValid()): ?>
 <form action="<?php echo url_for('user/resetPassword');?>" method="POST" name="ResetPasswordForm" id="ResetPasswordForm" class="ninjaForm"?>
   <fieldset>
+    <h1 class="log_title">Passwort vergessen?</h1>
     <?php if(isset($action_message)):?>
       <p class="error"><?php echo $action_message; ?></p>
     <?php endif;?>
