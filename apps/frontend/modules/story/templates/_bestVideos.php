437e8c25d53baa4f818156dc17d2bea53c838a6a
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-05 09:46:37 +0000

    bug #34004

--- apps/frontend/modules/story/templates/_bestVideos.php
+++ apps/frontend/modules/story/templates/_bestVideos.php
@@ -1,7 +1,7 @@
 <?php if(false !== $video):?>
   <?php echo $sf_data->get('video', ESC_RAW)->getSizedCode($width,$height); ?>
 <?php else:?>
-  <div class="sidead" style="width: auto !important; display: block !important;">
+  <div class="sidead" style="width: auto !important; display: block !important; float:left;">
     <!-- rectangle -->
     <div id='div-gpt-ad-1346766539123-0' style='width:300px; height:250px;'>
     <script type='text/javascript'>
