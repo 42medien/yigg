33f26a623a6005130d42b23a779036a58e2fcf9d
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-05 07:56:11 +0000

    bug #33596

--- lib/model/doctrine/Category.class.php
+++ lib/model/doctrine/Category.class.php
@@ -1,5 +1,14 @@
 <?php
 class Category extends BaseCategory
 {
+    public function getCategorySlug()
+    {
+        // replace all non letters or digits by -
+        $text = preg_replace('/\W+/', '-', $this->getName());
 
+        // trim and lowercase
+        $text = strtolower(trim($text, '-'));
+
+        return $text;
+    }
 }
