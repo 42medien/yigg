b5146f307bb85846ec58a1a01ca4f29947b4f63b
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-06 14:21:17 +0000

    bug #34179

--- apps/frontend/modules/worldspy/templates/showNodeSuccess.php
+++ apps/frontend/modules/worldspy/templates/showNodeSuccess.php
@@ -16,10 +16,11 @@
     ?></li>
   </ul>
 <?php endif; ?>
+<div class="story-list-cont">
 <ol class="story-list hfeed">
 <?php include_partial("expandedNode", array("entry" => $node))?>
 </ol>
-
+</div>
 <?php slot("sidebar_sponsoring")?>
    <?php echo include_component("sponsoring","sponsoring", array( 'place_id' => 23 ) ); ?>
 <?php end_slot()?>
