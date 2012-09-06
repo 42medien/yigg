80e40738619774e8d2923d82672b22ce3ce5a80a
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-06 13:58:54 +0000

    bug #34179

--- apps/frontend/modules/user/templates/myYiggSuccess.php
+++ apps/frontend/modules/user/templates/myYiggSuccess.php
@@ -1,6 +1,7 @@
 <?php include_partial("system/systemMessages")?>
 
 <?php if(count($stories) > 0): ?>
+<div class="story-list-cont">
   <ol id="story-list" class="story-list hfeed">
     <?php foreach($stories as $k => $story ): ?>
       <?php
@@ -16,6 +17,7 @@
       ?>
     <?php endforeach; ?>
   </ol>
+</div>
   <?php echo $pager->display(); ?>
 <?php else: ?>
 <p class="error">Es wurden keine Nachrichten gefunden</p>
