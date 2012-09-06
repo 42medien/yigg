f724b7de82c84c8f79e854856130b81e7b509a22
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-06 13:26:06 +0000

    bug #33441

--- apps/frontend/modules/story/actions/actions.class.php
+++ apps/frontend/modules/story/actions/actions.class.php
@@ -20,7 +20,7 @@ class storyActions extends yiggActions
 
         $this->relatedStories = StoryTable::retrieveRelatedStories($this->story);
 
-        //$this->getResponse()->setTitle($this->story->getTitle());
+        $this->getResponse()->setTitle($this->story->getTitle());
         $this->setLayout('layout.bar');
     }
 
