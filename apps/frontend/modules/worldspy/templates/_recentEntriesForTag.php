76c3667615c6e3c0da69b3c610ecf5e94dd0f39d
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-06 14:20:45 +0000

    bug #34179

--- apps/frontend/modules/worldspy/templates/_recentEntriesForTag.php
+++ apps/frontend/modules/worldspy/templates/_recentEntriesForTag.php
@@ -1,7 +1,9 @@
 <?php if(count($entries) > 0):?>
+<div class="story-list-cont">
   <ol class="story-list hfeed">
     <?php foreach($entries as $entry):?>
       <?php include_partial("worldspy/expandedNode", array("entry" => $entry)); ?>
     <?php endforeach; ?>
   </ol>
+</div>
 <?php endif;?>
