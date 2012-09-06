c2ed053e41b01113e6c22ca836c9b23791671fc4
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-06 12:31:53 +0000

    bug #34138

--- apps/frontend/modules/video/templates/_Youtube.php
+++ apps/frontend/modules/video/templates/_Youtube.php
@@ -1,3 +1,3 @@
 <object type="application/x-shockwave-flash" style="width:<?php echo $width;?>px; height:<?php echo $height;?>px;" data="http://www.youtube.com/v/<?php echo $video->getVideoId();?>">
-  <param name="movie" value="http://www.youtube.com/v/<?php echo $video->getVideoId();?>" />
+  <param name="movie" value="http://www.youtube.com/v/<?php echo $video->getVideoId();?>&showinfo=0" />
 </object>
