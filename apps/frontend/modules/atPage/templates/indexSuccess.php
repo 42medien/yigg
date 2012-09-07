83e73d15836f2770f53353435c7035472d482238
gplop <gplop@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-07 13:06:27 +0000

    bug #34082

--- apps/frontend/modules/atPage/templates/indexSuccess.php
+++ apps/frontend/modules/atPage/templates/indexSuccess.php
@@ -47,6 +47,7 @@
   <?php include_partial("user/avatarList", array("users" => $following));?>
 
   <h3 class="help_icon">Private Mitteilung schreiben</h3>
+      <?php print_r($reply_pm);?>
   <form <?php if(isset($reply_pm)): ?>id="pm_<?php echo $reply_pm->id; ?>_reply"<?php endif; ?> method="post" action="<?php echo url_for( "@notification_index"); ?>" class="post-box ninjaForm ninjaAjaxSubmit <?php if(isset($reply_pm)): ?>pm_<?php echo $reply_pm->id; ?>_reply<?php endif?>">
     <?php if( true === $sf_request->isMethod("post")):?>
        <?php if(isset($pm) && $pm->isValid() ): ?>
