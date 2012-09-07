c7d71c682ce6ae7e44db8ec09d22e456b6ec32e0
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-07 09:37:32 +0000

    bug #34170

--- apps/frontend/modules/system/templates/_navigation.php
+++ apps/frontend/modules/system/templates/_navigation.php
@@ -29,4 +29,11 @@
   <?php endif; ?>
 </ul>
 
-
+<ul id="categories-nav">
+    <h2>Kategorien</h2>
+    <?php $categories = Doctrine_Core::getTable('Category')->getCategories(); if(count($categories)):?>
+    <?php foreach($categories as $category):?>
+        <li><?php echo link_to($category->getName(), 'category_stories', $category); ?></li>
+        <?php endforeach;?>
+    <?php endif;?>
+</ul>
