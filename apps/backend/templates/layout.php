3ad017ae698043feaa5b27b476d60c525725805f
icovali <icovali@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-12 08:00:20 +0000

    bug #34342

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -17,6 +17,9 @@
      <link href="http://yigg.it/css/yigg-styles-backend.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
+   <div class="bg_top">
+   <div class="bg_bt">
+   <div class="bg_mid">
     <div id="container">
       <div class="header">
         <a tabindex="1" href="#Content" class="hidden">Direkt zum Inhalt </a>
@@ -95,6 +98,9 @@
        <div class="clr bth"><!--  clearer --></div>
       </div>
     </div>
+  </div>
+  </div>
+  </div>
 
     <?php if(false === $sf_request->isMobile()): ?>
       <script type="text/javascript" src="<?php echo sfConfig::get('static_host')?>js/prototype.js"></script>
