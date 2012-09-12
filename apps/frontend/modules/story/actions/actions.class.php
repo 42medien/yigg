87dacf77a85f94626c4b450fdd4ed4f5c68f1109
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-12 12:47:17 +0000

    bug #34438

--- apps/frontend/modules/story/actions/actions.class.php
+++ apps/frontend/modules/story/actions/actions.class.php
@@ -70,7 +70,7 @@ class storyActions extends yiggActions
     {
       $sf->confineWithMarkedForFrontpage();
     }
-    $sf->sortByYTTCS();
+    $sf->sortByDate();
     $query = $sf->getQuery();
     $query->groupBy("s.id");
 
