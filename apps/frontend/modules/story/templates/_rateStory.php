39e349fc0c44b808ed50dfcea4ce5d5581177fd4
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-14 09:02:23 +0000

    bug #34344

--- apps/frontend/modules/story/templates/_rateStory.php
+++ apps/frontend/modules/story/templates/_rateStory.php
@@ -53,6 +53,18 @@
           <h4><label></label><?php echo $story->currentRating() ?></h4>
         </div>
       <?php endif; ?>
+      
+      
+      <br> <br>
+              <style type="text/css" media="screen"> 
+          .yiggbutton { float:left; padding:3px 5px 5px 5px; } 
+      </style> 
+      
+      <div class="yiggbutton"> 
+          <script> yigg_url = 'URL DER NACHRICHT'; </script> 
+          <script src="http://static.yigg.de/v6/js/embed_flat_button.js"></script> 
+      </div>
+      
 <?php if(false === $sf_request->isAjaxRequest() && isset($external) && $external == true): ?>
     </body>
   </html>
