0df857c1b4bde3979191294a8baecf9ee3af005f
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-14 09:46:44 +0000

    bug #34344

--- lib/yiggStoryFinder.php
+++ lib/yiggStoryFinder.php
@@ -840,7 +840,9 @@ class yiggStoryFinder
     $this->time_from = null;
     $this->time_until = null;
 
-    $this->sorters['yttcs'] = "s.yttcs DESC, s.created_at DESC";
+    $this->sorters['yttcs'] = "s.yttcs DESC";
+    $this->sorters['created_at'] = "s.created_at DESC";
+    
     return $this;
   }
 
