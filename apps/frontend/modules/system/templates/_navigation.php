0f78c3064880ccc3bfa3f669c378f2152dd7b435
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-15 10:27:20 +0000

    bug #34438

--- apps/frontend/modules/system/templates/_navigation.php
+++ apps/frontend/modules/system/templates/_navigation.php
@@ -33,7 +33,7 @@
     <h3>Kategorien</h3>
     <?php $categories = Doctrine_Core::getTable('Category')->getCategories(); if(count($categories)):?>
     
-    var_dump($categories);
+    <?php var_dump($categories); ?>
     
     <?php foreach($categories as $category):?>
         <li><?php echo link_to($category->getName(), 'category_stories', $category); ?></li>
