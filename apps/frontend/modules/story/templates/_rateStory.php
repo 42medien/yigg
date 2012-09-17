14e31d32f7e94d0fb6e95c9cdbc25fe1114a13db
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-17 08:54:46 +0000

    bug #34344

--- apps/frontend/modules/story/templates/_rateStory.php
+++ apps/frontend/modules/story/templates/_rateStory.php
@@ -6,7 +6,24 @@
       <base href="http://<?php echo $sf_request->getHost() . $sf_request->getRelativeUrlRoot();  ?>/" />
       <style>
         * { padding:0; margin:0;}
-      
+      <?php if( !isset($flat) || isset($flat) && $flat === false ): ?>
+        body{ font-family:Arial,Helvetica,sans-serif; font-size:62.5%; color:#3F352F; position:relative;   font-size: 11px;}
+        a img, fieldset{ border:none}
+       .rating-form{ float:left;  margin-top:4px; text-align:center; background:/*url(images/newbutton.png) no-repeat*/ transparent;  color:#fff;  overflow:hidden;  width:200px;  height:54px; }
+/*     .rating-form h4{ font-size:1.9em !important;  overflow:hidden;  padding-right:6px;  text-align:right; margin:0 !important}*/
+       .rating-form p{ margin:0;  padding-right:6px; text-align:right}
+       .rating-form fieldset{ padding:0px}
+       .rating-form input,  .voted span{ display:inline; background:none;  border:0; line-height:1.7em;  text-align:center;  color:#105B1B;  cursor:pointer}
+       .voted{ background:/*url(images/newbuttonvoted.png) no-repeat 1px;*/ transparent;}
+       .voted span, .rating-form input {display:block; background: #ffc851; border: 1px solid #c7c7c7; border-radius: 3px; float: left; line-height: 18px; padding: 0 2px;}
+       .rating-form input, .voted span, .voted span a {text-decoration: none; color: #000;}
+       .voted h4 label, .rating-form h4 label {-moz-transform: rotate(-45deg); 
+                                               -webkit-transform: rotate(-45deg); /* Safari 3.1+ и Chrome 2.0+ */
+                                                -o-transform: rotate(-45deg); /* Opera 10.5+ */
+                                                -ms-transform: rotate(-45deg); /* IE 9.0 */
+                             background: #FFFFFF; border-color: #CCCCCC transparent transparent #CCCCCC; border-style: solid; border-width: 1px; display: block; height: 3px; left: -3px; position: absolute; top: 6px; width: 3px;}
+       .voted h4, .rating-form h4 {color: #666666; float: left; font-weight: normal; text-align: center; margin-left: 5px; padding: 2px 3px; position: relative; background: none repeat scroll 0 0 #FFFFFF; border: 1px solid #CCCCCC; border-radius: 3px 3px 3px 3px;}
+      <?php else: ?>
         body{ font-family:Arial,Helvetica,sans-serif; font-size:62.5%; color:#3F352F; position:relative;   font-size: 11px;}
         a img, fieldset{ border:none}
         .rating-form fieldset, form .rating-form.voted, .rating-form.voted{ padding-left:2px; background: url(images/toolbar-digits-left.gif) 0% 50% no-repeat}
@@ -15,7 +32,7 @@
         .rating-form.voted span{ float:left; background: url(images/toolbar-yiggit.gif) 0% 50% no-repeat; border:0; height:20px; width:50px; font-size:11px; line-height:20px; text-align:center; color:#105B1B; cursor:pointer}
         .rating-form.voted span{ color:white; cursor:none; background: url(images/toolbar-yigged.gif) 0% 50% repeat-x}
         .button {border: 0;}
-      
+      <?php endif; ?>
        </style>
     </head>
   <body>
