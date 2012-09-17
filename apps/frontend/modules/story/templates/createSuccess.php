5eba92cdf97a08852c888c7b7439d6b49292986d
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-17 15:24:16 +0000

    bug #34946

--- apps/frontend/modules/story/templates/createSuccess.php
+++ apps/frontend/modules/story/templates/createSuccess.php
@@ -1,4 +1,12 @@
 
+<script type="text/javascript">
+    $('*').ready(function() {
+        //if($("#external_url").val()){
+            $('#external_url').focus().select();
+            $('#external_url').blur();
+        //}
+    });
+</script>
 <?php use_stylesheet('carousel.css') ?>
 <form action="<?php echo url_for($story->getStoryType() === "Normal" ? "@story_create" : "@story_create_article");?>" id="new_form" class="ninjaForm <?php echo ("Normal" === $view ? "": "article" ); ?>" <?php if(true === $form->isMultipart()):?>enctype="multipart/form-data"<?php endif; ?> method="post">
   <fieldset>
