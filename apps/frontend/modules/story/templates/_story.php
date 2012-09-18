29fd405d57bc8244351a41f66e1cf5c6ed76eb20
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-18 14:20:39 +0000

    bug #35038

--- apps/frontend/modules/story/templates/_story.php
+++ apps/frontend/modules/story/templates/_story.php
@@ -163,7 +163,7 @@ jQuery("img.js-resize").ready(function(){
 <div class="story_bt_data">
     <?php if($sf_request->getModuleAction() === "story/show"):?>
     <div class="spreadly-button">
-         <a href="<?php echo urlencode($sf_request->getUriPrefix().url_for_story($story, false));?>&social=0&color=ff9500&title=<?php echo urlencode($story->title);?>" 
+         <a href="<?php echo urlencode($sf_request->getUriPrefix().url_for_story($story, false));?>" 
             target="_blank"
             rel="like">
          </a>
