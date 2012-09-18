90d260280cbbaeeaa860cd177b26fdb23a790324
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-18 09:44:24 +0000

    bug #34946

--- apps/frontend/modules/story/templates/createSuccess.php
+++ apps/frontend/modules/story/templates/createSuccess.php
@@ -30,7 +30,7 @@
         //alert($j("#Description").val()); //object is properly initialized
         //$j('#Description').focus().select();
         $j('#external_url').focus().select();
-        $j('#external_url').mouseleave();
+        $j('#external_url').focusout();
         
         //$j('#Title').focus().select();
         
