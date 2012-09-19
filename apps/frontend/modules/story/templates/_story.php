ff895ae0368a312996abbf1ba07393c2e95b5bdb
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-19 12:32:18 +0000

    bug #34823

--- apps/frontend/modules/story/templates/_story.php
+++ apps/frontend/modules/story/templates/_story.php
@@ -218,7 +218,7 @@ jQuery("img.js-resize").ready(function(){
                 title: "<?php echo $js_title; ?>",
                 url: "<?php echo $js_url; ?>",
                 text: "<?php echo $js_text; ?>",
-                img: "https://yigg.yopeso.com/htdocs/images/uploads/images/stories/story-2639353.jpg"
+                img: "<?php echo $js_img; ?>"
             });
 
             PLISTA.partner.init();
