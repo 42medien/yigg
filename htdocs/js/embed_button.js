87867a4afb0afc846ffefaa32ebcb893483dacd0
acucer <acucer@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-24 11:03:43 +0000

    #35362

--- htdocs/js/embed_button.js
+++ htdocs/js/embed_button.js
@@ -1,4 +1,4 @@
 (function(){
 	var u = yigg_url ? yigg_url : YIGG_URL;
-	document.write("<iframe src='http://yigg.yopeso.com/nachrichten/flatbutton?url=" + escape(u) + "' height='60' width='100' frameborder='0' scrolling='no'></iframe>");
+	document.write("<iframe src='http://yigg.de/nachrichten/flatbutton?url=" + escape(u) + "' height='60' width='100' frameborder='0' scrolling='no'></iframe>");
 })()
