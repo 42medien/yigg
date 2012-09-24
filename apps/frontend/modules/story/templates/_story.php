aa5464f52cc717723b47f79366c415f36abdb57a
acucer <acucer@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-09-24 14:17:02 +0000

    #35460

--- apps/frontend/modules/story/templates/_story.php
+++ apps/frontend/modules/story/templates/_story.php
@@ -180,7 +180,7 @@ jQuery("img.js-resize").ready(function(){
       <?php include_component("comment", "commentList", array("obj" => $story, "inlist" => isset($inlist)  ? $inlist : false)); ?>
     <?php if($sf_request->getModuleAction() === "story/show"):?>       
 		<?php if($sf_user->isAuthenticated()): ?><br/><br/><?php endif; ?>
-		  <div style="width: 100% !important;">
+		  <div style="width: 100% !important; height: 30px !important;">
            <span class="hlp_txt_spread">Teile die News mit Deinen Freunden!</span>
            <iframe class="spreadly-button" frameBorder="0" scrolling="no" style="border: 0; height: 29px;" src="http://button.spread.ly/?url=<?php echo urlencode($sf_request->getUriPrefix().url_for_story($story, false)) ?>"></iframe>
            <?php /*<a href="<?php echo $sf_request->getUriPrefix().url_for_story($story, false);?>" 
