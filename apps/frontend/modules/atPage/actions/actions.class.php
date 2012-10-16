d2917b32521e51771845c09d82f50580444f9553
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-10-16 13:39:49 +0000

    bug #37710

--- apps/frontend/modules/atPage/actions/actions.class.php
+++ apps/frontend/modules/atPage/actions/actions.class.php
@@ -10,7 +10,7 @@ class atPageActions extends yiggActions
   public function executeIndex($request)
   {
     $this->form = new FormPostboxSimpleCreate();
-    $this->ajax_pm = "Nachricht gesendet!";
+    //$this->ajax_pm = "Nachricht gesendet!";
     if(false !== $request->getParameter("id",false))
     {
       $this->reply_pm = Doctrine::getTable("NotificationMessage")->findOneById(intval($request->getParameter("id")));
