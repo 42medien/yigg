a5e42acd2222df0163519a98c4cd273deab81ffb
amoscaliuc <amoscaliuc@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-10-17 08:45:23 +0000

    bug #37709

--- apps/frontend/modules/story/templates/_story.php
+++ apps/frontend/modules/story/templates/_story.php
@@ -8,9 +8,9 @@
        <?php echo link_to($story->title, url_for_story($story, "bar"), array("title" => $story->title, 'target' => '_blank'));?>
      <?php else:?>
        <?php
-            //if(strlen($story->title) >= 75)
-                //$story_title = substr($story->title, 0, 75)." ...";
-            //else
+            if(strlen($story->title) >= 75)
+                $story_title = preg_replace('/\s+?(\S+)?$/', '', substr($story->title, 0, 75))." ...";
+            else
                 $story_title = $story->title;
             //echo link_to_story(preg_replace('/\s+?(\S+)?$/', '', $story_title), $story, array("title" => $story->title));
             echo link_to_story($story_title, $story, array("title" => $story->title));
