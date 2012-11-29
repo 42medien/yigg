1482166baadd87f71918d9aafdab6e73b5466c17
aturcanu <aturcanu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-11-29 08:41:12 +0000

    bug #40933

--- apps/frontend/modules/story/templates/_createStoryButton.php
+++ apps/frontend/modules/story/templates/_createStoryButton.php
@@ -78,7 +78,7 @@
                     $protocol_array = explode("/", $_SERVER['SERVER_PROTOCOL']);
                     $protocol = strtolower($protocol_array[0]);	
                     $url = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-                    echo link_to("ZYiGG", "@story_create", array("target" => "_blank", "query_string" => "exturl={$url}"));
+                    echo link_to("YiGG", "@story_create", array("target" => "_blank", "query_string" => "exturl={$url}"));
                 ?>
             </span>
             <h4><label></label>0</h4>
