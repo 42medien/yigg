ec674c53fb8c827c56a6776de32d907f08fccd8b
aturcanu <aturcanu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-12-03 15:34:10 +0000

    bug #40933

--- htdocs/js/embed_flat_button.js
+++ htdocs/js/embed_flat_button.js
@@ -1,4 +1,5 @@
 (function(){
-	var u = yigg_url ? yigg_url : YIGG_URL;
+	//var u = yigg_url ? yigg_url : YIGG_URL;
+        var u = document.URL;
 	document.write("<iframe src='http://yigg.de/nachrichten/button?url=" + escape(u) + "' height='20' width='77' frameborder='0' scrolling='no'></iframe>");
 })()
