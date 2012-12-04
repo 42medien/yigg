6f67dac95e58d9f983599d02c35f266febec311b
aturcanu <aturcanu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-12-04 09:33:15 +0000

    bug #41833 disabled VibrantAds

--- apps/frontend/modules/system/templates/_javascript.php
+++ apps/frontend/modules/system/templates/_javascript.php
@@ -1,6 +1,6 @@
 <script type="text/javascript" src="/js/protoaculous_min.js"></script>
 <script type="text/javascript" src="/js/ninjitsu_min.js?v=2"></script>
-<?php include_component('system','vibrantAds'); ?>
+<?php //include_component('system','vibrantAds'); ?>
 <script type="text/javascript">
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-19326817-1']);
