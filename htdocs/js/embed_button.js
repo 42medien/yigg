13a6ad61cdfbd93e115a04f1d8c8720963efdf76
aturcanu <aturcanu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-12-05 10:06:15 +0000

    bug #40933

--- htdocs/js/embed_button.js
+++ htdocs/js/embed_button.js
@@ -1,5 +1,4 @@
 (function(){
-//	var u = yigg_url ? yigg_url : YIGG_URL;
-        var u = document.URL;
+	var u = yigg_url ? yigg_url : document.URL;
 	document.write("<iframe src='http://yigg.de/nachrichten/flatbutton?url=" + escape(u) + "' height='74' width='100' frameborder='0' scrolling='no'></iframe>");
 })()
--- htdocs/js/embed_flat_button.js
+++ htdocs/js/embed_flat_button.js
@@ -1,5 +1,4 @@
 (function(){
-	//var u = yigg_url ? yigg_url : YIGG_URL;
-        var u = document.URL;
+	var u = yigg_url ? yigg_url : document.URL;
 	document.write("<iframe src='http://yigg.de/nachrichten/button?url=" + escape(u) + "' height='20' width='77' frameborder='0' scrolling='no'></iframe>");
 })()
