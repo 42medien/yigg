d642ef5222a9695c25b7195d91330a2b0d147829
aturcanu <aturcanu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-12-11 12:05:25 +0000

    bug #40933

--- htdocs/js/embed_button.js
+++ htdocs/js/embed_button.js
@@ -1,4 +1,7 @@
 (function(){
 	var u = yigg_url ? yigg_url : document.URL;
-	document.write("<iframe src='http://yigg.de/nachrichten/flatbutton?url=" + escape(u) + "' height='74' width='100' frameborder='0' scrolling='no'></iframe>");
+        if (yigg_url.substring(0, 1) == "/" || yigg_url.substring(0,3) == "%2F") {
+            u = window.location.origin + yigg_url;
+        }
+	document.write("<iframe src='http://yigg.local/nachrichten/flatbutton?url=" + escape(u) + "' height='74' width='100' frameborder='0' scrolling='no'></iframe>");
 })()
--- htdocs/js/embed_flat_button.js
+++ htdocs/js/embed_flat_button.js
@@ -1,4 +1,7 @@
 (function(){
 	var u = yigg_url ? yigg_url : document.URL;
-	document.write("<iframe src='http://yigg.de/nachrichten/button?url=" + escape(u) + "' height='20' width='77' frameborder='0' scrolling='no'></iframe>");
+        if (yigg_url.substring(0, 1) == "/" || yigg_url.substring(0,3) == "%2F") {
+            u = window.location.origin + yigg_url;
+        }
+	document.write("<iframe src='http://yigg.local/nachrichten/button?url=" + escape(u) + "' height='20' width='77' frameborder='0' scrolling='no'></iframe>");
 })()
