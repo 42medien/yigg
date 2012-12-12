5d8355b760c93bccf07a0d88dfa7f0a57318be3a
aturcanu <aturcanu@dfbb8578-c1e2-42b4-a4bd-35dbbfd7a451>
2012-12-12 12:52:34 +0000

    bug #40933

--- htdocs/js/embed_button.js
+++ htdocs/js/embed_button.js
@@ -1,7 +1,7 @@
 (function(){
 	var u = yigg_url ? yigg_url : document.URL;
         if (yigg_url.substring(0, 1) == "/" || yigg_url.substring(0,3) == "%2F") {
-            u = window.location.origin + yigg_url;
+            u = window.location.protocol + "//" + window.location.host + yigg_url;
         }
 	document.write("<iframe src='http://yigg.de/nachrichten/flatbutton?url=" + escape(u) + "' height='74' width='100' frameborder='0' scrolling='no'></iframe>");
 })()
