c22981b0491c469da21071209e7410a689bc33b6
pfefferle <pfefferle@gmail.com>
2013-04-15 14:00:32 +0200

    some syntax fixes

--- apps/frontend/modules/user/templates/myYiggError.php
+++ apps/frontend/modules/user/templates/myYiggError.php
@@ -6,8 +6,8 @@
 <h3>Was kann ich tun?</h3>
 <p class="note">Sobald du anderen YiGGern folgst, kannst Du hier ihre Nachrichten mitverfolgen.</p>
 
-<?php slot("sidebar")?>
+<?php slot("sidebar") ?>
   <?php echo include_component("sponsoring","sponsoring", array( 'place_id' => 27 ) ); ?>
   <div id="friendsWidget"><?php include_partial("userWidgetFollowing", array("following" => $following, "view" => "online"));?></div>
   <?php include_partial("userWidgetStats", array("user" => $user));?>
-<?end_slot()?>
+<?php end_slot() ?>
