483d1632dd9ac80af69f9c8d2311f86550ce9f0d
pfefferle <pfefferle@gmail.com>
2013-04-15 15:16:29 +0200

    more html5

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xml:lang="de" xmlns="http://www.w3.org/1999/xhtml">
   <head profile='http://www.w3.org/2006/03/hcard'>
     <script type='text/javascript'>
