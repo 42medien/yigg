dd8dc971383e7d7adc5fd4696f9481781ace0400
pfefferle <pfefferle@gmail.com>
2013-08-21 15:15:10 +0200

    added some more of the old nav points

--- apps/frontend/modules/system/templates/_main_navigation.php
+++ apps/frontend/modules/system/templates/_main_navigation.php
@@ -6,9 +6,15 @@
     <li <?php if($sf_request->getAction() == 'newStories'): ?>class="selected"<?php endif;?>>
       <?php echo link_to('Neueste Nachrichten',"@new_stories","title=Neueste Nachrichten von heute"); ?>
     </li>
+    <li <?php if($sf_request->getAction() == 'spy'): ?>class="selected"<?php endif;?>>
+      <?php echo link_to('Spion',"@spy","title=Neueste Nachrichten von heute"); ?>
+    </li>
+    <li <?php if($sf_request->getAction() == 'worldSpy'): ?>class="selected"<?php endif;?>>
+      <?php echo link_to('Weltspion',"@worldspy","title=Neueste Nachrichten von heute"); ?>
+    </li>
     <li <?php if($sf_request->getAction() == 'archive'): ?>class="selected"<?php endif;?>>
       <?php echo link_to('Archiv',"@story_archive","title=Neueste Nachrichten von heute"); ?>
-    </li>  
+    </li>
     <li class="create"><?php echo link_to('Nachricht erstellen', '@story_create', array('title' => 'Neue Nachricht erstellen', 'class' => 'button')); ?></li>
   </ol>
 </div>
