e3ef7ad4c27548c0dcdb6b93f6929607ceb38900
pfefferle <pfefferle@gmail.com>
2013-08-27 12:36:08 +0200

    archive menu

--- apps/frontend/modules/story/templates/archiveSuccess.php
+++ apps/frontend/modules/story/templates/archiveSuccess.php
@@ -1,4 +1,10 @@
-
+<nav class="filter-list">Sortieren nach:
+  <?php echo link_to("Datum", "spy/spy", array("query_string" => "show=all")); ?>,
+  <?php echo link_to("letzte 24 Stunden", "spy/spy", array("query_string" => "show=new")); ?>,
+  <?php echo link_to("zuletzt ge-yigged", "spy/spy", array("query_string" => "show=top-rated")); ?>,
+  <?php echo link_to("zuletzt kommentiert", "spy/spy", array("query_string" => "show=latest-comments")); ?>,
+  <?php echo link_to("zuletzt angeschaut", "spy/spy", array("query_string" => "show=last-read")); ?>
+</nav>
 
 <?php if(isset($stories)&& count($stories) > 0): ?>
 <div class="story-list-cont">
