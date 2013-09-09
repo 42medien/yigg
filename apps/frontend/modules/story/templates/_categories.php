12d0fd114b31f318533f8df1728c55733c834156
pfefferle <pfefferle@gmail.com>
2013-09-09 16:48:29 +0200

    nicer html

--- apps/frontend/modules/story/templates/_categories.php
+++ apps/frontend/modules/story/templates/_categories.php
@@ -1,11 +1,13 @@
 <?php if(count($categories) > 0):?>
 <section id="widget-categories" class="widget">
   <h2>Kategorien</h2>
-
+  
+  <ul>
   <?php
   foreach($categories as $category) {
-    echo link_to($category->name, "@category_stories?id=".$category->id."&category_slug=".$category->name);
+    echo "<li>" . link_to($category->name, "@category_stories?id=".$category->id."&category_slug=".$category->name) . "</li>";
   }
   ?>
+  </ul>
 </section>
 <?php endif;?>
