e4138cde42a0dfef784278cd8e2328f76d1ae80a
pfefferle <pfefferle@gmail.com>
2013-09-13 13:52:22 +0200

    new widget order

--- apps/frontend/modules/system/templates/_sidebar.php
+++ apps/frontend/modules/system/templates/_sidebar.php
@@ -1,13 +1,43 @@
+<?php
+if (true === $sf_user->hasUser()) {
+  include_partial("user/userinfo");
+}
+?>
+
+<?php if(true === $sf_request->hasParameter("rss")): ?>
+<section id="widget-feed">
+  <h2>Seite abonnieren</h2>
+  <ul>
+    <li>
+<?php
+  echo link_to(
+    " Atom-Feed",
+    $sf_request->forceParams(
+      array(
+        "sf_format" => "atom"
+      )
+    ),
+    array(
+      'title' => 'Diese Seite als Atom-Feed abonnieren',
+      'class' => 'rss atom feed icon-rss-sign',
+      'rel' => 'feed'
+    )
+  );
+?>
+    </li>
+    <li>
+      <input type="button" onclick="(function(){var z=document.createElement('script');z.src='https://www.subtome.com/load.js';document.body.appendChild(z);})()" value="Subscribe" />
+    </li>
+  </ul>
+</section>
+<?php endif; ?>
+
 <section id="widget-search">
   <?php include_component("search", "form"); ?>
 </section>
 
 <?php
-  if (true === $sf_user->hasUser()) {
-    include_partial("user/userinfo");
-  }
-
-  if (has_slot("sidebar")) {
-    include_slot("sidebar");
-  }
+if (has_slot("sidebar")) {
+  include_slot("sidebar");
+}
 ?>
