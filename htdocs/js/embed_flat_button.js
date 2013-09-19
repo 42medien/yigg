fc116086a7c2026c23c90148c956eb10b46ebfe9
pfefferle <pfefferle@gmail.com>
2013-09-19 15:07:51 +0200

    cleaner way to load button iframe

--- htdocs/js/embed_flat_button.js
+++ htdocs/js/embed_flat_button.js
@@ -3,5 +3,5 @@
         if (yigg_url.substring(0, 1) == "/" || yigg_url.substring(0,3) == "%2F") {
             u = window.location.protocol + "//" + window.location.host + yigg_url;
         }
-	document.write("<iframe src='http://yigg.de/nachrichten/button?url=" + escape(u) + "' height='20' width='77' frameborder='0' scrolling='no'></iframe>");
+	document.write("<iframe src='http://yigg.de/nachrichten/button?flat=true&url=" + escape(u) + "' height='27' width='110' frameborder='0' scrolling='no'></iframe>");
 })()
