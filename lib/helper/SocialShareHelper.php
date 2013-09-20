0434f5ca943da19974da0fda0e05de12efdfa8ba
pfefferle <pfefferle@gmail.com>
2013-09-20 16:41:41 +0200

    added xing, linkedin and pinterest

--- lib/helper/SocialShareHelper.php
+++ lib/helper/SocialShareHelper.php
@@ -8,6 +8,9 @@ function social_counter($url, $options = array()) {
   $google_count = $smc->get_google();
   $yigg_count = $smc->get_yigg();
   $spreadly_count = $smc->get_spreadly();
+  $xing_count = $smc->get_xing();
+  $linkedin_count = $smc->get_linkedin();
+  $pinterest_count = $smc->get_pinterest();
 ?>
 <ul class="social-share-counter">
   <li class="yigg"><span class="counter"><?php echo $yigg_count; ?></span> yiggs</li>
@@ -15,6 +18,9 @@ function social_counter($url, $options = array()) {
   <li class="twitter"><span class="counter"><?php echo $twitter_count; ?></span> tweets</li>
   <li class="facebook"><span class="counter"><?php echo $facebook_count; ?></span> likes</li>
   <li class="google"><span class="counter"><?php echo $google_count; ?></span> +1s</li>
+  <li class="xing"><span class="counter"><?php echo $xing_count; ?></span> <abbr title="XING">xs</abbr></li>
+  <li class="linkedin"><span class="counter"><?php echo $linkedin_count; ?></span> <abbr title="LinkedIn">ins</abbr></li>
+  <li class="pinterest"><span class="counter"><?php echo $pinterest_count; ?></span> pins</li>
 </ul>
 <?php
 }
@@ -22,7 +28,7 @@ function social_counter($url, $options = array()) {
 function social_counter_simple($url, $options = array()) {
   $smc = new yiggSocialNetworkCounter($url);
   
-  $counter = $smc->get_facebook() + $smc->get_twitter() + $smc->get_google() + $smc->get_yigg() + $smc->get_spreadly();
+  $counter = $smc->get_facebook() + $smc->get_twitter() + $smc->get_google() + $smc->get_yigg() + $smc->get_spreadly() + $smc->get_xing() + $smc->get_linkedin() + $smc->get_pinterest();
 ?>
 <div class="social-share-counter">Insgesamt <span class="counter"><?php echo $counter; ?></span> mal geteilt</div>
 <?php
--- lib/yiggSocialNetworkCounter.php
+++ lib/yiggSocialNetworkCounter.php
@@ -61,4 +61,40 @@ class yiggSocialNetworkCounter {
     
     return 0;
   }
+  
+  public function get_xing() {
+    $raw_data = yiggUrlTools::do_get('https://www.xing-share.com/app/share?op=get_share_button;url=' . urlencode($this->url) . ';counter=right;lang=de;type=iframe;hovercard_position=1;shape=square');
+    
+    if (preg_match("/<span class=\"xing-count right\">(.*?)<\/span>/si", $raw_data, $matches)) {
+      return intval($matches[1]);
+    }
+
+    return 0;
+  }
+  
+  public function get_linkedin() {
+    $raw_data = yiggUrlTools::do_get('http://www.linkedin.com/countserv/count/share?url=' . urlencode($this->url) . '&format=json');
+    
+    if ($data = json_decode($raw_data, true)) {
+      if (array_key_exists('count', $data)) {
+        return intval($data['count']);
+      }
+    }
+    
+    return 0;
+  }
+  
+  public function get_pinterest() {
+    $raw_data = yiggUrlTools::do_get('http://api.pinterest.com/v1/urls/count.json?callback=receiveCount&url=' . urlencode($this->url));
+    
+    if (preg_match("/receiveCount\((.*?)\)/si", $raw_data, $matches)) {
+      if ($data = json_decode($matches[1], true)) {
+        if (array_key_exists('count', $data)) {
+          return intval($data['count']);
+        }
+      }
+    }
+    
+    return 0;
+  }
 }
