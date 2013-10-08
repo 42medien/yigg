c39ae808b931bdb029b0c3bae13c25efaaa13df4
pfefferle <pfefferle@gmail.com>
2013-10-08 11:02:31 +0200

    changed to html5

--- config/error/error.html.php
+++ config/error/error.html.php
@@ -1,6 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xml:lang="de" xmlns="http://www.w3.org/1999/xhtml">
-  <head profile='http://www.w3.org/2006/03/hcard'>
+<!DOCTYPE html>
+<html>
+  <head>
     <link rel="shortcut icon" href="/favicon.ico" />
 
     <!-- metas -->
