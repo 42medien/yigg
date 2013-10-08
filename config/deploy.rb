b7e706ef8786a83a07c087919f8773fc058309b7
pfefferle <pfefferle@gmail.com>
2013-10-08 17:42:50 +0200

    updated capistrano stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,35 +1,24 @@
-default_run_options[:pty] = true
-set :application, "yigg"
-set :use_sudo,    false
+set :application, 'yigg'
+set :repo_url, 'git@github.com:ekaabo/yigg.git'
 
-set :repository,  "git@github.com:ekaabo/yigg.git"
-set :scm,         "git"
-set :user,        "spreadly"
+set :branch, 'main'
+
+set :deploy_to, '/mnt/data/yigg/www/httpdocs'
+set :scm, :git
+set :user, "spreadly"
 set :scm_passphrase, "affen2010"
-set :branch,      "master"
 
-role :web,        "yigg.de"                       # Your HTTP server, Apache/etc
-set :current_dir, "current"
-ssh_options[:forward_agent] = true
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
-set :keep_releases,  5
+set :forward_agent, true
 
-task :staging do
-  set :sf_env,    "staging"
-  set :domain,    "staging.yigg.de"
-  set :deploy_to, "/mnt/data/yigg/www/httpdocs"
-  set :deploy_via, :checkout
-  puts "Deploying #{application} to #{domain} for env=#{sf_env} …"
-end
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# Symfony stuff
-
-before "deploy:symlink"
-after "deploy:symlink"
-
-
-# Dirs that need to remain the same between deploys (shared dirs)
-set :shared_children,   %w(log web/uploads)
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
 
 namespace :deploy do
   desc "Overwrite the start task because symfony doesn't need it."
@@ -53,81 +42,11 @@ namespace :deploy do
   task :update do
     transaction do
       update_code
-      symfony.yiid.build
-      symlink
+      #symfony.yiid.build
+      #symlink
     end
   end
 
-  desc "Symlink static directories and static files that need to remain between deployments."
-  task :share_childs do
-    if shared_children
-      shared_children.each do |link|
-        run "mkdir -p #{shared_path}/#{link}"
-        run "if [ -d #{release_path}/#{link} ] ; then rm -rf #{release_path}/#{link}; fi"
-        run "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"
-      end
-    end
-  end
+  after :finishing, 'deploy:cleanup'
 
-  desc "Customize the finalize_update task to work with symfony."
-  task :finalize_update, :except => { :no_release => true } do
-    run "mkdir -p #{latest_release}/cache"
-    run "mkdir -p #{latest_release}/log"
-    run "chmod -R 777 #{latest_release}/cache"
-    run "chmod -R 755 #{latest_release}/log"
-
-    # Share common files & folders
-    share_childs
-  end
-
-  desc "Need to overwrite the deploy:cold task so it doesn't try to run the migrations."
-  task :cold do ; end
-
-end
-
-namespace :symfony do
-
-  desc "Clear the cache."
-  task :cc do
-    run "php #{latest_release}/symfony cc --env=#{sf_env}"
-  end
-
-  desc "Disable the app."
-  task :disable do
-    run "php #{latest_release}/symfony project:disable #{sf_env}"
-  end
-
-  desc "Enable the app."
-  task :enable do
-    run "php #{latest_release}/symfony project:enable #{sf_env}"
-  end
-
-  namespace :yigg do
-    desc "Build it."
-    task :build, :roles => :web do
-      command = "php #{latest_release}/symfony yigg:build --all --env=#{sf_env} --no-confirmation"
-
-      do_it = Capistrano::CLI.ui.ask("Do you really want to do this:\n#{command}\nAnswer with (y|n)[n]: ")
-
-      if do_it=='y'
-        run command
-      else
-        puts "Skipping it"
-      end
-    end
-  end
-end
-
-# Runs +command+ as root invoking the command with su -c
-# and handling the root password prompt.
-#
-#   surun "/etc/init.d/apache reload"
-#   # Executes
-#   # su - -c '/etc/init.d/apache reload'
-#
-def surun(command)
-  password = fetch(:root_password, Capistrano::CLI.password_prompt("root password: "))
-  run("su - -c '#{command}'", :options => {:pty => true}) do |channel, stream, output|
-    channel.send_data("#{password}n") if output
-  end
 end
