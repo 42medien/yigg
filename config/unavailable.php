cbf01bade82446f080c2946c643363baa9cc1f75
pfefferle <pfefferle@gmail.com>
2013-10-08 11:22:44 +0200

    updated the maintenance site to the new design

--- config/unavailable.php
+++ config/unavailable.php
@@ -1,54 +1,80 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xml:lang="de" xmlns="http://www.w3.org/1999/xhtml">
-  <head profile='http://www.w3.org/2006/03/hcard'>
+<!DOCTYPE html>
+<html>
+  <head>
     <link rel="shortcut icon" href="/favicon.ico" />
 
     <!-- metas -->
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="title" content="Nachrichten zum Mitmachen - YiGG.de" />
-	<meta name="robots" content="index, follow" />
-	<meta name="description" content="YiGG.de - Auf YiGG kannst Du Nachrichten lesen, bewerten, kommentieren und eigene Artikel verfassen. Bei YiGG entscheiden die Benutzer selbst, was interessant ist." />
+	  <meta name="robots" content="index, follow" />
+	  <meta name="description" content="YiGG.de - Auf YiGG kannst Du Nachrichten lesen, bewerten, kommentieren und eigene Artikel verfassen. Bei YiGG entscheiden die Benutzer selbst, was interessant ist." />
 
-	<meta name="keywords" content="YiGG.de, YiGG, social news, Nachrichten, Bookmarks, Diskussionen, Neuigkeiten, Unterhaltung, Technik, Hamster, Web 2.0, Marvin, German News, Germany" />
-	<meta name="language" content="de" />
+	  <meta name="keywords" content="YiGG.de, YiGG, social news, Nachrichten, Bookmarks, Diskussionen, Neuigkeiten, Unterhaltung, Technik, Hamster, Web 2.0, Marvin, German News, Germany" />
+	  <meta name="language" content="de" />
     <title>YiGG ist momentan nicht erreichbar.</title>
 
     <!-- styles -->
-    <link href="css/yigg-styles-v8.css" rel="stylesheet" type="text/css" />
+    <link href="/css/v9/styles.css" rel="stylesheet" type="text/css" />
   </head>
-  <body>
-    <div id="container">
-      <div id="content">
-        <div class="twoThree">
-         <div class="twoThree-left">
-         <h1>YiGG ist momentan nicht erreichbar. Wir überarbeiten das System und sind in Kürze wieder da.</h1>
+  <body class="error-page">
+    <div id="page">
+      <header id="branding">
+        <nav id="access" role="navigation">
+          <a tabindex="1" href="#content" class="skip-link screen-reader-text">Direkt zum Inhalt </a>
+          <a href="/" title="YiGG Nachrichten zum Mitmachen: Lesen - Bewerten - Schreiben" rel="home" class="logo">
+            <img src="/images/yigg_logo.png" alt="YiGG Nachrichten zum Mitmachen: Lesen - Bewerten - Schreiben" height="53" width="90" />
+          </a>
+        </nav>
+      </header>
+      
+      <div id="main">
+        <main id="content" class="h-entry hentry page" itemscope itemtype="http://schema.org/WebPage">
+          <h1 class="page-title p-name entry-title" itemprop="name headline">YiGG ist momentan nicht erreichbar. Wir überarbeiten das System und sind in Kürze wieder da.</h1>
+          
+          <article class="e-description entry-description" itemprop="description text">
+            <p class="alert alert-danger error">YiGG ist im Wartungsmodus und wird in Kürze wieder erreichbar sein.</p>
 
-              <p class="alert alert-danger error">YiGG ist im Wartungsmodus und wird in Kürze wieder erreichbar sein.</p>
+            <object width="100%" height="485"><param name="movie" value="http://www.youtube.com/v/DWx-NYeNvVA&amp;hl=de_DE&amp;fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/DWx-NYeNvVA&amp;hl=de_DE&amp;fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="100%" height="485"></embed></object>
 
-              <object width="100%" height="485"><param name="movie" value="http://www.youtube.com/v/DWx-NYeNvVA&amp;hl=de_DE&amp;fs=1"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="http://www.youtube.com/v/DWx-NYeNvVA&amp;hl=de_DE&amp;fs=1" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="100%" height="485"></embed></object>
-
-              <p class="alert alert-info note">Wenn Du magst komme einfach in einigen Minuten wieder :)</p>
-
-           </div>
-          <div class="twoThree-right">
-            <img src="http://static.yigg.de/v6/images/404.gif" />
-          </div>
+            <p class="alert alert-info note">Wenn Du magst komme einfach in einigen Minuten wieder :)</p>
+            
+            <img src="/images/404.gif"/>
+          </article>
         </div>
       </div>
-      </div>
+     
+      <footer class="site-footer">
+        <div class="site-footer-widgets">
+          <div class="about widget">
+            <h4>Über YiGG</h4>
+            <ul>
+              <li><a href="/ueber-uns/kontakt">Kontakt</a></li>
+              <li><a href="/ueber-uns/impressum">Impressum</a></li>
+              <li><a href="http://blog.yigg.de" target="_blank">YiGG-Blog</a></li>
+            </ul>
+          </div>
+          <div class="legal widget">
+            <h4>Nutzungsbestimmungen</h4>
+            <ul class="end">
+              <li><a href="/nutzungsbedingungen/verhaltensrichtlinien">Verhaltensrichtlinien</a></li>
+              <li><a href="/nutzungsbedingungen/nutzungsbedingungen">Nutzungsbedingungen</a></li>
+              <li><a href="/nutzungsbedingungen/datenschutzrichtlinien">Datenschutzrichtlinien</a></li>
+            </ul>
+          </div>
+      </footer>
     </div>
-  </div>    <!--  Goole Analytics Code -->
-      <script type="text/javascript">/*<![CDATA[*/
-        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-      /*]]>*/</script>
-      <script type="text/javascript">/*<![CDATA[*/
-        var pageTracker = _gat._getTracker("UA-5634155-2");
-        pageTracker._initData();
-        pageTracker._trackPageview();
-       /*]]>*/</script>
+    
+    <!--  Goole Analytics Code -->
+    <script type="text/javascript">/*<![CDATA[*/
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    /*]]>*/</script>
+    <script type="text/javascript">/*<![CDATA[*/
+      var pageTracker = _gat._getTracker("UA-5634155-2");
+      pageTracker._initData();
+      pageTracker._trackPageview();
+     /*]]>*/</script>
 
-    <!-- End of Google Analytics Code -->
-
+  <!-- End of Google Analytics Code -->
   </body>
 </html>
