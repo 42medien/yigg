4f48b25c7ca09964edf623bd17eb171dd64d1d1f
pfefferle <pfefferle@gmail.com>
2013-10-08 10:57:17 +0200

    updated error page to the new design

--- config/error/error.html.php
+++ config/error/error.html.php
@@ -14,39 +14,68 @@
     <title>YiGG.de - Anwendungsfehler 500</title>
 
     <!-- styles -->
-    <link href="css/yigg-styles-v8.css" rel="stylesheet" type="text/css" />
+    <link href="/css/v9/styles.css" rel="stylesheet" type="text/css" />
   </head>
-  <body>
-    <div id="container">
-      <div id="content">
-        <div class="twoThree">
-          <div class="twoThree-left">
-              <h1>Bei deiner letzten Anfrage ist leider ein Fehler aufgetreten.</h1>
+  
+  <body class="error-page">
+    <div id="page">
+      <header id="branding">
+        <nav id="access" role="navigation">
+          <a tabindex="1" href="#content" class="skip-link screen-reader-text">Direkt zum Inhalt </a>
+          <a href="/" title="YiGG Nachrichten zum Mitmachen: Lesen - Bewerten - Schreiben" rel="home" class="logo">
+            <img src="/images/yigg_logo.png" alt="YiGG Nachrichten zum Mitmachen: Lesen - Bewerten - Schreiben" height="53" width="90" />
+          </a>
+        </nav>
+      </header>
+    
+      <div id="main">
+        <main id="content" class="h-entry hentry page" itemscope itemtype="http://schema.org/WebPage">
+          <h1 class="page-title p-name entry-title" itemprop="name headline">Bei deiner letzten Anfrage ist leider ein Fehler aufgetreten.</h1>
+          
+          <article class="e-description entry-description" itemprop="description text">
+            <h2>Was kann ich tun?</h2>
+            <p class="alert alert-success">Hilf uns den Fehler schnell zu finden, indem Du <a href="/ueber-uns/bugreport">hier</a> klickst!</p>
 
-              <h2>Was kann ich tun?</h2>
-              <p class="alert alert-success">Hilf uns den Fehler schnell zu finden, indem Du <a href="http://yigg.de/ueber-uns/bugreport">hier</a> klickst!</p>
-
-              <h2>Und dann?</h2>
-              <p class="alert alert-info note">Es besteht eine hohe Wahrscheinlichkeit, dass der Rest der Seite normal funktioniert. Probiere doch in der Zwischenzeit ein paar andere Funktionen aus.</p>
-          </div>
-          <div class="twoThree-right">
-              <img src="http://static.yigg.de/v6/images/404.gif"/>
-          </div>
-        </div>
+            <h2>Und dann?</h2>
+            <p class="alert alert-info note">Es besteht eine hohe Wahrscheinlichkeit, dass der Rest der Seite normal funktioniert. Probiere doch in der Zwischenzeit ein paar andere Funktionen aus.</p>
+            
+            <img src="/images/404.gif"/>
+          </article>
+        </main>                
       </div>
-      </div>
+      
+      <footer class="site-footer">
+        <div class="site-footer-widgets">
+          <div class="about widget">
+            <h4>Über YiGG</h4>
+            <ul>
+              <li><a href="/ueber-uns/kontakt">Kontakt</a></li>
+              <li><a href="/ueber-uns/impressum">Impressum</a></li>
+              <li><a href="http://blog.yigg.de" target="_blank">YiGG-Blog</a></li>
+            </ul>
+          </div>
+          <div class="legal widget">
+            <h4>Nutzungsbestimmungen</h4>
+            <ul class="end">
+              <li><a href="/nutzungsbedingungen/verhaltensrichtlinien">Verhaltensrichtlinien</a></li>
+              <li><a href="/nutzungsbedingungen/nutzungsbedingungen">Nutzungsbedingungen</a></li>
+              <li><a href="/nutzungsbedingungen/datenschutzrichtlinien">Datenschutzrichtlinien</a></li>
+            </ul>
+          </div>
+      </footer>
+    </div>
+    
+    <!--  Goole Analytics Code -->
+    <script type="text/javascript">/*<![CDATA[*/
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    /*]]>*/</script>
+    <script type="text/javascript">/*<![CDATA[*/
+      var pageTracker = _gat._getTracker("UA-5634155-2");
+      pageTracker._initData();
+      pageTracker._trackPageview();
+     /*]]>*/</script>
 
-      <!--  Goole Analytics Code -->
-      <script type="text/javascript">/*<![CDATA[*/
-        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-      /*]]>*/</script>
-      <script type="text/javascript">/*<![CDATA[*/
-        var pageTracker = _gat._getTracker("UA-5634155-2");
-        pageTracker._initData();
-        pageTracker._trackPageview();
-       /*]]>*/</script>
-
-    <!-- End of Google Analytics Code -->
+  <!-- End of Google Analytics Code -->
   </body>
 </html>
