4d40758c41ef1633029343e712b7204808870cb1
pfefferle <pfefferle@gmail.com>
2013-10-10 10:07:06 +0200

    added viewport

--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -1,3 +1,4 @@
+<meta name="viewport" content="width=device-width" />
 <link rel="shortcut icon" href="/favicon.png" />
 <link rel="profile" href="http://microformats.org/profile/specs" />
 <link rel="profile" href="http://microformats.org/profile/hatom" />
