8f16c20f090fc774494f1fccfe0b24fc4c154e52
pfefferle <pfefferle@gmail.com>
2013-10-18 08:56:54 +0200

    try to fix twitter urls

--- apps/frontend/modules/story/templates/_twitterShares.php
+++ apps/frontend/modules/story/templates/_twitterShares.php
@@ -1,14 +1,14 @@
 <?php if (count($tweets->getRawValue()) > 0) { ?>
 <section id="widget-tweets" class="widget">
   <h2 class="heading-right">Passende Tweets</h2>
-  <ul class="avatar-list">                 
+  <ul class="avatar-list">
     <?php foreach($tweets->getRawValue() as $tweet_res) { ?>
     <li>
-      <?php 
+      <?php
         $twitter_username = htmlSpecialChars($tweet_res->user->screen_name);
-        $twitter_status = htmlSpecialChars($tweet_res->id);
+        $twitter_status = $tweet_res->id;
         $tweer_url = 'https://twitter.com/' . $twitter_username . '/status/' . $twitter_status;
-            
+
         echo link_to(
                img_tag(
                  htmlSpecialChars($tweet_res->user->profile_image_url),
