9351cbcea297a89f19729308bd8f56be08ca5f9c
pfefferle <pfefferle@gmail.com>
2013-10-22 10:56:19 +0200

    change host

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -15,7 +15,7 @@ role :web, %w{www.yigg.de}
 # definition into the server list. The second argument
 # something that quacks like a has can be used to set
 # extended properties on the server.
-server 'www.yigg.de', user: 'ec2-user', roles: %w{app web}
+server 'yigg_web', user: 'ec2-user', roles: %w{app web}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
