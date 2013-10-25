b16775f524c874988642e31d3187c59232a68820
pfefferle <pfefferle@gmail.com>
2013-10-25 11:22:06 +0200

    use only the big networks

--- lib/helper/SocialShareHelper.php
+++ lib/helper/SocialShareHelper.php
@@ -2,15 +2,12 @@
 
 function social_counter($url, $options = array()) {
   $smc = new yiggSocialNetworkCounter($url);
-  
+
   $facebook_count = $smc->get_facebook();
   $twitter_count = $smc->get_twitter();
   $google_count = $smc->get_google();
   $yigg_count = $smc->get_yigg();
   $spreadly_count = $smc->get_spreadly();
-  $xing_count = $smc->get_xing();
-  $linkedin_count = $smc->get_linkedin();
-  $pinterest_count = $smc->get_pinterest();
 ?>
 <ul class="social-share-counter">
   <li class="yigg"><span class="counter"><?php echo $yigg_count; ?></span> yiggs</li>
@@ -18,17 +15,14 @@ function social_counter($url, $options = array()) {
   <li class="twitter"><span class="counter"><?php echo $twitter_count; ?></span> tweets</li>
   <li class="facebook"><span class="counter"><?php echo $facebook_count; ?></span> likes</li>
   <li class="google"><span class="counter"><?php echo $google_count; ?></span> +1s</li>
-  <li class="xing"><span class="counter"><?php echo $xing_count; ?></span> <abbr title="XING">xs</abbr></li>
-  <li class="linkedin"><span class="counter"><?php echo $linkedin_count; ?></span> <abbr title="LinkedIn">ins</abbr></li>
-  <li class="pinterest"><span class="counter"><?php echo $pinterest_count; ?></span> pins</li>
 </ul>
 <?php
 }
 
 function social_counter_simple($url, $options = array()) {
   $smc = new yiggSocialNetworkCounter($url);
-  
-  $counter = $smc->get_facebook() + $smc->get_twitter() + $smc->get_google() + $smc->get_yigg() + $smc->get_spreadly() + $smc->get_xing() + $smc->get_linkedin() + $smc->get_pinterest();
+
+  $counter = $smc->get_facebook() + $smc->get_twitter() + $smc->get_google() + $smc->get_yigg() + $smc->get_spreadly();
 ?>
 <div class="social-share-counter">Insgesamt <span class="counter"><?php echo $counter; ?></span> mal geteilt</div>
 <?php
