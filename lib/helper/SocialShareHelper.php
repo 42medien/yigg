6c9864c8a7806b96cb9df0e7f3a0fbe46c75a160
pfefferle <pfefferle@gmail.com>
2013-11-06 18:23:36 +0100

    removed spreadly

--- lib/helper/SocialShareHelper.php
+++ lib/helper/SocialShareHelper.php
@@ -6,11 +6,9 @@ function social_counter($url, $options = array()) {
   $facebook_count = $smc->get_facebook();
   $twitter_count = $smc->get_twitter();
   $yigg_count = $smc->get_yigg();
-  $spreadly_count = $smc->get_spreadly();
 ?>
 <ul class="social-share-counter">
   <li class="yigg"><span class="counter"><?php echo $yigg_count; ?></span> yiggs</li>
-  <li class="spreadly"><span class="counter"><?php echo $spreadly_count; ?></span> shares</li>
   <li class="twitter"><span class="counter"><?php echo $twitter_count; ?></span> tweets</li>
   <li class="facebook"><span class="counter"><?php echo $facebook_count; ?></span> likes</li>
 </ul>
@@ -20,7 +18,7 @@ function social_counter($url, $options = array()) {
 function social_counter_simple($url, $options = array()) {
   $smc = new yiggSocialNetworkCounter($url);
 
-  $counter = $smc->get_facebook() + $smc->get_twitter() + $smc->get_yigg() + $smc->get_spreadly();
+  $counter = $smc->get_facebook() + $smc->get_twitter() + $smc->get_yigg();
 ?>
 <div class="social-share-counter">Insgesamt <span class="counter"><?php echo $counter; ?></span> mal geteilt</div>
 <?php
