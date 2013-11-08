c998661f4b3c1d056a479e0e8ebd908b88e54ed2
pfefferle <pfefferle@gmail.com>
2013-11-08 17:14:13 +0100

    fixed feeds

--- apps/frontend/modules/story/templates/bestStoriesSuccess.atom.php
+++ apps/frontend/modules/story/templates/bestStoriesSuccess.atom.php
@@ -2,8 +2,11 @@
 <feed xmlns="http://www.w3.org/2005/Atom">
    <id>http://yigg.de/atom/beste-nachrichten</id>
    <title>Die besten Nachrichten</title>
-   <author><name>Die YiGG Nutzer</name></author>
+   <author>
+     <name>Die YiGG Nutzer</name>
+    </author>
    <link href="<?php echo url_for("@best_stories", true);?>" />
    <updated><?php echo date(DATE_ATOM, strtotime($stories->getLast()->created_at)); ?></updated>
+
    <?php include_partial("story/listAtom", array("stories" => $stories)); ?>
 </feed>
--- apps/frontend/modules/story/templates/newStoriesSuccess.atom.php
+++ apps/frontend/modules/story/templates/newStoriesSuccess.atom.php
@@ -1,7 +1,9 @@
 <?php echo'<?xml version="1.0" encoding="utf-8"?>'?>
-<feed xmlns="http://www.w3.org/2005/Atom" rel="self">
-<id>tag:yigg.de,2009-10-1:neueste-nachrichten</id>
-<title>Alle neu eingestellten Beiträge der letzten 24 Stunden</title>
-<link href="<?php echo url_for("@new_stories", true);?>" />
-<updated><?php echo date(DATE_ATOM, strtotime($stories->getFirst()->created_at)); ?></updated>
-<?php include_partial("story/listAtom", array("stories" => $stories));
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <id>tag:yigg.de,2009-10-1:neueste-nachrichten</id>
+  <title>Alle neu eingestellten Beiträge der letzten 24 Stunden</title>
+  <link href="<?php echo url_for("@new_stories", true);?>" />
+  <updated><?php echo date(DATE_ATOM, strtotime($stories->getFirst()->created_at)); ?></updated>
+
+  <?php include_partial("story/listAtom", array("stories" => $stories)); ?>
+</feed>
