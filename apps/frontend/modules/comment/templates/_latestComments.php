de794b0c18c49259e4d39994662e0f572de8ef0c
pfefferle <pfefferle@gmail.com>
2013-11-11 10:37:35 +0100

    nicer comment lists

--- apps/frontend/modules/comment/templates/_latestComments.php
+++ apps/frontend/modules/comment/templates/_latestComments.php
@@ -4,11 +4,9 @@
   <ol class="avatar-list-style">
   <?php foreach ($comments as $comment): ?>
     <li>
-      <?php
-        $story = $comment->getParentObj();
-        echo link_to(avatar_tag($comment->Author->Avatar, "icon.gif", 14,14), "@user_public_profile?username={$comment->Author->username}");
-        echo link_to($story['title'], $story->getLink(ESC_RAW), array("title"=> "Lies die Kommentare zu {$story['title']}","class" => "title"));
-      ?>
+      <?php $story = $comment->getParentObj(); ?>
+      <?php echo link_to(avatar_tag($comment->Author->Avatar, "icon.gif", 14,14), "@user_public_profile?username={$comment->Author->username}"); ?>
+      <?php echo link_to($story['title'], $story->getLink(ESC_RAW), array("title"=> "Lies die Kommentare zu {$story['title']}","class" => "title")); ?>
     </li>
   <?php endforeach; ?>
   </ol>
