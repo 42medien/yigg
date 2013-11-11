152159b6e8f8f53c78366edeaf4906ab23422d64
pfefferle <pfefferle@gmail.com>
2013-11-11 15:42:57 +0100

    some small css fixes

--- apps/frontend/modules/sponsoring/templates/_story_single.php
+++ apps/frontend/modules/sponsoring/templates/_story_single.php
@@ -1,5 +1,5 @@
 <!-- yigg_news_details -->
-<div id='div-gpt-ad-1384179156582-0' style='width:728px; height:90px;'>
+<div id='div-gpt-ad-1384179156582-0' style='width:728px; height:90px; margin-bottom: 50px;'>
   <script type='text/javascript'>
     googletag.cmd.push(function() { googletag.display('div-gpt-ad-1384179156582-0'); });
   </script>
