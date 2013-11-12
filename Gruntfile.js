74acba43cd6e2a60ca830fed7e369dd69aab1d14
pfefferle <pfefferle@gmail.com>
2013-11-12 15:31:34 +0100

    updated grunt to do the combine and minify stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,19 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    concat: {
+      dist: {
+        src: [
+          'htdocs/js/ninjaCommander.js',
+          'htdocs/js/ninjaComs.js',
+          'htdocs/js/ninjaValidator.js',
+          'htdocs/js/ninjaUpdater.js',
+          'htdocs/js/ninjaAction.js',
+          'htdocs/js/ninja.js'
+        ],
+        dest: 'htdocs/js/ninjitsu.js',
+      },
+    },
     sass: {                              // Task
       dist: {                            // Target
         options: {                       // Target options
@@ -14,10 +27,20 @@ module.exports = function(grunt) {
         }
       }
     },
+    uglify: {
+      dist: {
+        options: {
+          mangle: true
+        },
+        files: {
+          'htdocs/js/ninjitsu_min.js': ['htdocs/js/ninjitsu.js']
+        }
+      }
+    },
     watch: {
       styles: {
         files: ['**/*.scss'],
-        tasks: ['sass']
+        tasks: ['sass', 'concat', 'uglify']
       }
     }
   });
@@ -25,8 +48,10 @@ module.exports = function(grunt) {
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Default task.
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.registerTask('default', ['sass', 'concat', 'uglify', 'watch']);
 
 };
