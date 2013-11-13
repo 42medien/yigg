6c44aa883fe23a6311e91461e4dd967df3cf914c
pfefferle <pfefferle@gmail.com>
2013-11-13 18:04:17 +0100

    styled ad-header

--- apps/frontend/modules/sponsoring/templates/_story.php
+++ apps/frontend/modules/sponsoring/templates/_story.php
@@ -1,6 +1,6 @@
 <?php use_helper("Date", "Text"); ?>
 <article class="post story-advertising" id="advertising-1384260153932">
-  Anzeige
+  <div class="ad-label">Anzeige</div>
   <!-- yigg_big_kachel_336x280 -->
   <div class="ad-container" id='div-gpt-ad-1384260153932-0' style='width:336px; height:280px;'>
     <script type='text/javascript'>
