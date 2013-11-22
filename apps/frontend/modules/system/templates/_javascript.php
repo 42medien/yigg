a660e3949585447ef880351d44c701897f977cee
pfefferle <pfefferle@gmail.com>
2013-11-22 13:13:28 +0100

    changed google analtics code

--- apps/frontend/modules/system/templates/_javascript.php
+++ apps/frontend/modules/system/templates/_javascript.php
@@ -2,36 +2,13 @@
 <script type="text/javascript" src="/js/protoaculous_min.js"></script>
 <script type="text/javascript" src="/js/ninjitsu_min.js?v=2"></script>
 <script type="text/javascript">
-  var googletag = googletag || {};
-  googletag.cmd = googletag.cmd || [];
-  (function() {
-    var gads = document.createElement('script');
-    gads.async = true;
-    gads.type = 'text/javascript';
-    var useSSL = 'https:' == document.location.protocol;
-    gads.src = (useSSL ? 'https:' : 'http:') +
-    '//www.googletagservices.com/tag/js/gpt.js';
-    var node = document.getElementsByTagName('script')[0];
-    node.parentNode.insertBefore(gads, node);
-  })();
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  googletag.cmd.push(function() {
-    googletag.defineSlot('/1007584/yigg_startseite_zwischen', [320, 50], 'div-gpt-ad-1384169965681-0').addService(googletag.pubads());
-    googletag.pubads().enableSingleRequest();
-    googletag.enableServices();
-  });
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-19326817-1']);
-  _gaq.push(['_trackPageview']);
-  _gaq.push(['_gat._anonymizeIp']);
-  _gaq.push(['_trackPageLoadTime']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+  ga('create', 'UA-19326817-1', 'yigg.de');
+  ga('send', 'pageview');
 
   var _sf_async_config={uid:23222,domain:"yigg.de"};
   (function(){
